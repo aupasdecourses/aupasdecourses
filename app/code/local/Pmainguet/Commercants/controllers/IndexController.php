9351bb7e526676d71cedd0822d31b3656665ffe1
pmainguet <mainguetpierre@gmail.com>
2016-04-30 19:25:26 +0200

    Finalisation automatisation création nouvelle boutique

--- app/code/local/Pmainguet/Commercants/controllers/IndexController.php
+++ app/code/local/Pmainguet/Commercants/controllers/IndexController.php
@@ -12,6 +12,54 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
     private $_codeboutique = 'saintmartin';
     private $_nameboutique = 'Saint Martin';
 
+    private $_commercant = [
+            'Boucher' => 'Boucherie Lévêque',
+            'Primeur' => 'Verger Saint-Martin',
+            'Fromager' => 'Fromagerie Bouvet',
+            'Poissonnier' => 'Les Viviers de Noirmoutier',
+            'Caviste' => 'La cave du marché Saint-Martin',
+            'Boulanger' => 'La Miche Qui Fume',
+            ];
+
+    private $_amasty = [
+             'batiment',
+             'codeporte1',
+             'codeporte2',
+             'contactvoisin',
+             'etage',
+             'infoscomplementaires',
+             'produit_equivalent',
+             'telcontact',
+         ];
+
+    private $_pagetoupdate = [
+            'annonce-livreurs',
+            'autoriser-cookies',
+            'besoin-d-aide',
+            'no-route',
+            'mentions-legales-cgv',
+            'nos-engagements',
+            'politique-confidentialite-restriction-cookie',
+            'tarifs-livraison',
+            'zone-et-horaire',
+        ];
+
+    private $_blocktoupdate = [
+            'footer_links_middle',
+            'footer_links_left',
+            'cookie_restriction_notice_block',
+            'footer_links_right',
+            'footer_links_middle_2',
+            'footer-social',
+            'footer-cards',
+        ];
+
+    private $_titlehomepage = 'Au Pas De Courses - Livraison de courses Saint Martin Paris 10e et 3e';
+    private $_metadescription ='Les livraisons de courses dans le 10e et 3e arrondissements avec Au Pas De Courses, pour bien manger avec les meilleurs commerçants de Paris. Essayez ce soir, c\'est facile!';
+    private $_content_heading = 'Au Pas De Courses Saint Martin, votre livraison de produits frais dans le 10e et 3e';
+
+    private $_shippingruletoduplicate = 'Restriction 17e';
+
     public function indexAction()
     {
         $this->loadLayout();
@@ -21,6 +69,8 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
     //Creation de la Root Category
     public function createrootcatAction()
     {
+        echo '//Création d’une catégorie racine pour le magasin ////</br></br>';
+
         $storeId = 0;
         $category = Mage::getModel('catalog/category');
         $category->setStoreId($storeId);
@@ -38,15 +88,22 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
     //Creation de la Boutique, du Group Store et du Store
     public function createstoreAction()
     {
+        echo '//Création du Website, Group Store et Store ////</br></br>';
+
         //To create from the frontend
         Mage::registry('isSecureArea');
+
         //#addWebsite
         /** @var $website Mage_Core_Model_Website */
         $website = Mage::getModel('core/website');
-        $website->setCode($this->_codewebsite)
+        if (is_null($website->load($this->_codewebsite)->getWebsiteId())) {
+            $website->setCode($this->_codewebsite)
             ->setName($this->_namewebsite)
             ->save();
-        echo 'Website '.$this->_namewebsite.' created!</br>';
+            echo 'Website '.$this->_namewebsite.' created!</br>';
+        } else {
+            echo 'Website '.$this->_namewebsite.' already exist. Stopped!</br>';
+        }
 
         //get root category id
         $category = Mage::getResourceModel('catalog/category_collection')
@@ -57,57 +114,84 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
         //#addStoreGroup
         /** @var $storeGroup Mage_Core_Model_Store_Group */
         $storeGroup = Mage::getModel('core/store_group');
-        $storeGroup->setWebsiteId($website->getId())
+        $check = $storeGroup->getCollection()->addFieldToFilter('name', $this->_namestoregroup)->getFirstItem()->getGroupId();
+        if (is_null($check)) {
+            $storeGroup->setWebsiteId($website->getId())
             ->setName($this->_namestoregroup)
             ->setRootCategoryId($categoryId)
             ->save();
-        echo 'Store Group '.$this->_namestoregroup.' created!</br>';
+            echo 'Store Group '.$this->_namestoregroup.' created!</br>';
+        } else {
+            echo 'Store Group '.$this->_namestoregroup.' already exist. Stopped!</br>';
+        }
 
         //#addStore
         /** @var $store Mage_Core_Model_Store */
         $store = Mage::getModel('core/store');
-        $store->setCode($this->_codeboutique)
+        $check = $store->getCollection()->addFieldToFilter('code', $this->_codeboutique)->getFirstItem()->getStoreId();
+        if (is_null($check)) {
+            $store->setCode($this->_codeboutique)
             ->setWebsiteId($storeGroup->getWebsiteId())
             ->setGroupId($storeGroup->getId())
             ->setName($this->_nameboutique)
             ->setIsActive(1)
             ->save();
-        echo 'Store '.$this->_nameboutique.' created!</br>';
+            echo 'Store '.$this->_nameboutique.' created!</br>';
+        } else {
+            echo 'Store '.$this->_nameboutique.' already exist. Stopped!</br>';
+        }
     }
 
     //Change Scope of existing CMS Home Page
     public function cmspageAction()
     {
+        echo '//Création et mise à jour contenu nouvelle boutique ////</br></br>';
+
         $modelid = intval(Mage::getConfig()->getNode('stores')->{$this->_codemodel}->{'system'}->{'store'}->{'id'});
         $newstoreid = intval(Mage::getConfig()->getNode('stores')->{$this->_codeboutique}->{'system'}->{'store'}->{'id'});
 
         //DUPLICATE HOMEPAGE
-        //Get home page of Model
-        $page= Mage::getModel('cms/page')->setStoreId($modelid)->load('home');
-        $cmsPageData=[
-            'title'=>'Au Pas De Courses - Livraison de courses Saint Martin Paris 10e et 3e',
-            'root_template'=>$page->getRootTemplate(),
-            'meta_description'=>'Les livraisons de courses dans le 10e et 3e arrondissements avec Au Pas De Courses, pour bien manger avec les meilleurs commerçants de Paris. Essayez ce soir, c\'est facile!',
-            'identifier'=>$page->getIdentifier(),
-            'content_heading'=>$page->getContentHeading(),
-            //'content'=>$page->getContent(),
-            'custom_root_template'=>$page->getCustomRootTemplate(),
-            'store_id'=>[$newstoreid],
-        ];
-        Mage::getModel('cms/page')->setData($cmsPageData)->save();
-        echo 'Homepage for '.$this->_storecode.' created!';
+        echo '//DUPLICATE HOMEPAGE//</br>';
+        $page = Mage::getModel('cms/page')->setStoreId($modelid)->load('home');
+        $check = sizeof(Mage::getModel('cms/page')->setStoreId($newstoreid)->load('home')->getData());
+        if ($check == 0) {
+            $cmsPageData = [
+                 'title' => $this->_titlehomepage,
+                 'root_template' => $page->getRootTemplate(),
+                 'meta_description' => $this->_metadescription,
+                 'identifier' => $page->getIdentifier(),
+                 'content_heading' => $this->_content_heading,
+                 //'content'=>$page->getContent(),
+                 'custom_root_template' => $page->getCustomRootTemplate(),
+                 'store_id' => [$newstoreid],
+            ];
+            Mage::getModel('cms/page')->setData($cmsPageData)->save();
+            echo 'Homepage for '.$this->_nameboutique.' created!</br>';
+        } else {
+            echo 'Homepage for '.$this->_nameboutique.' already exists. Next!</br>';
+        }
 
-        //UPDATE CAROUSSEL & FOOTER BLOCKS
-        $blocks = [
-            'carroussel-home-page',
-            'footer_links_middle',
-            'footer_links_left',
-            'cookie_restriction_notice_block',
-            'footer_links_right',
-            'footer_links_middle_2',
-            'footer-social',
-            'footer-cards',
-        ];
+        //DUPLICATE CAROUSSEL
+        echo '//DUPLICATE CARROUSSEL//</br>';
+        $b = Mage::getModel('cms/block')->load('carroussel-home-page');
+        $check = sizeof(Mage::getModel('cms/block')->load('carroussel-home-page-saintmartin')->getData());
+        if ($check == 0) {
+            $data = $b->setStoreId($modelid)->getData();
+            unset($data['block_id']);
+            unset($data['creation_time']);
+            $data['title'] = 'Caroussel Home Page '.$this->_nameboutique;
+            $data['store_id'] = $newstoreid;
+            $data['stores'] = array($newstoreid);
+            $b->setData($data);
+            $b->save();
+            echo 'Carroussel has been duplicated</br>';
+        } else {
+            echo 'Carroussel already exist. Next!</br>';
+        }
+
+        //UPDATE FOOTER BLOCKS
+        echo '//UPDATE FOOTER BLOCKS//</br>';
+        $blocks = $this->_blocktoupdate;
         foreach ($blocks as $block) {
             $b = Mage::getModel('cms/block')->setStoreId($modelid)->load($block);
             $arraystore = $b->getStoreId();
@@ -122,17 +206,8 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
         }
 
         //UPDATE EXISTING PAGES
-        $pages = [
-            'annonce-livreurs',
-            'autoriser-cookies',
-            'besoin-d-aide',
-            'no-route',
-            'mentions-legales-cgv',
-            'nos-engagements',
-            'politique-confidentialite-restriction-cookie',
-            'tarifs-livraison',
-            'zone-et-horaire',
-        ];
+        echo '//UPDATE EXISTING PAGES//</br>';
+        $pages = $this->_pagetoupdate;
         foreach ($pages as $page) {
             $b = Mage::getModel('cms/page')->setStoreId($modelid)->load($page);
             $arraystore = $b->getStoreId();
@@ -141,26 +216,109 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
                 $b->setStoreId($arraystore);
                 $b->save();
                 echo 'Page '.$page.' updated!</br>';
+            } else {
+                echo 'Page '.$page.' already OK.Next!</br>';
             }
-            echo 'Page '.$page.' already OK.Next!</br>';
         }
+    }
+
+    //Change Amasty Order Attributes
+    public function attributesAction()
+    {
+        echo '//// Setup Order Attributes, Restriction Shipping & Delivery Time ////</br></br>';
+
+        $modelid = intval(Mage::getConfig()->getNode('stores')->{$this->_codemodel}->{'system'}->{'store'}->{'id'});
+        $newstoreid = intval(Mage::getConfig()->getNode('stores')->{$this->_codeboutique}->{'system'}->{'store'}->{'id'});
+
+        //Amasty Order Attributes
+        echo '//UPDATE AMASTY ORDER ATTRIBUTES//</br>';
+        $amasty = $this->_amasty;
+
+        foreach ($amasty as $am) {
+            $attribute = Mage::getModel('eav/entity_attribute')->loadByCode(5, $am);
+            if (!in_array($newstoreid, explode(',', $attribute->getStoreIds()))) {
+                $attribute->setStoreIds($attribute->getStoreIds().','.$newstoreid);
+                $attribute->save();
+                echo 'Attribut '.$am.' updated!</br>';
+            } else {
+                echo 'Attribut '.$am.' already OK.Next!</br>';
+            }
+        }
+
+        //Duplicate Shipping restriction
+        echo '//DUPLICATE SHIPPING RESTRICTION//</br>';
+        $namerule = $this->_shippingruletoduplicate;
+        $namenewrule = 'Restriction '.$this->_nameboutique;
+
+        $check = Mage::getModel('amshiprestriction/rule')->getCollection()
+           ->addFieldToFilter('is_active', true)
+           ->addFieldToFilter('name', $namenewrule)->getFirstItem();
+
+        if (is_null($check->getRuleId())) {
+            $data = Mage::getModel('amshiprestriction/rule')->getCollection()
+               ->addFieldToFilter('is_active', true)
+               ->addFieldToFilter('name', $namerule)->getData()[0];
+            unset($data['rule_id']);
+            $data['stores'] = $newstoreid;
+            $data['name'] = $namenewrule;
+            $newrule = Mage::getModel('amshiprestriction/rule')->setData($data);
+            $newrule->save();
+            echo 'Shipping restriction '.$namenewrule.' created!</br>';
+        } else {
+            echo 'Shipping restriction '.$namenewrule.' already exist.Pass!</br>';
+        }
+
+        //Duplicate Delivery Slot
+        echo '//DUPLICATE DELIVERY SLOT//</br>';
+        $collection = Mage::getModel('ddate/dtime')->getCollection()
+            ->addFieldToFilter('store_id', array('eq' => $modelid));
+
+        foreach ($collection as $col) {
+            $check = Mage::getModel('ddate/dtime')->getCollection()
+                ->addFieldToFilter('dtime', $col->getDtime())
+                ->addFieldToFilter('store_id', array('eq' => $newstoreid))->getFirstItem();
+            if (is_null($check->getData('dtime_id'))) {
+                $data = $col->getData();
+                unset($data['dtime_id']);
+                unset($data['special_day']);
+                unset($data['store_id']);
+                $data['dtime_stores'] = array($newstoreid);
+                $check->setData($data);
+                $check->save();
+                echo 'Delivery Slot '.$col->getDtime().' created!</br>';
+            } else {
+                echo 'Delivery Slot '.$col->getDtime().' already exist.Pass!</br>';
+            }
+        }
+
+        //Duplicate entries for shipping rates
+        echo '//DUPLICATE ENTRIES FOR SHIPPING RATE --------------->>>>>>> DO IT BY HAND FOR THE MOMENT (IMPORTANT)!//</br>';
+        /*$rates = Mage::getResourceModel('shipping/carrier_tablerate_collection')
+            ->addFieldToFilter('website_id', $modelid);
+
+        foreach ($rates as $rat) {
+            $data = $rat->getData();
+            unset($data['pk']);
+            $data['website_id'] = $newstoreid;
+            //try {
+                $check=Mage::getModel('shipping/carrier_tablerate'); //Not functioning
+                $check->setData($data);
+                $check->save();
+            //} catch (Exception $e) {
+                //print_r($e);
+            //}
+        }
+        echo 'Shipping rates duplicated!';*/
     }
 
     //Création des catégories générales Commerçants
     public function creategeneralcatAction()
     {
-        $code = 'saintmartin';
-        $catnames = [
-            'Boucher',
-            'Primeur',
-            'Fromager',
-            'Poissonnier',
-            'Caviste',
-            'Boulanger',
-            //"Epicier"
-        ];
+        echo '//// Création de l\’arborescence initiale (catégories commercants) ////</br></br>';
 
-        $storeid = intval(Mage::getConfig()->getNode('stores')->{$code}->{'system'}->{'store'}->{'id'});
+        $catnames = array_keys($this->_commercant);
+
+        $storeid = intval(Mage::getConfig()->getNode('stores')->{$this->_codeboutique}->{'system'}->{'store'}->{'id'});
 
         $rootCategoryId = Mage::app()->getStore($storeid)->getRootCategoryId();
         foreach ($catnames as $name) {
@@ -181,7 +339,7 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
                 $parentCategory = Mage::getModel('catalog/category')->load($parentId);
                 $category->setPath($parentCategory->getPath());
                 $category->save();
-                echo 'Category created!</br>';
+                echo 'Category '.$name.' created!</br>';
             } else {
                 echo 'Category '.$name.' already exist. Next!</br>';
             }
@@ -191,17 +349,12 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
     //Create commercants catégories and product attributes
     public function createcommercantcatAction()
     {
-        $code = 'saintmartin';
-        $catnames = [
-            'Boucher' => 'Boucherie Lévêque',
-            'Primeur' => 'Verger Saint-Martin',
-            'Fromager' => 'Fromagerie Bouvet',
-            'Poissonnier' => 'Les Viviers de Noirmoutier',
-            'Caviste' => 'La cave du marché Saint-Martin',
-            'Boulanger' => 'La cave du marché Saint-Martin',
-            ];
-          //create attribute option if do not exist
-          $arg_attribute = 'commercant';
+        echo '//// Création des catégories et attributs commerçants sous Magento ////</br></br>';
+
+        $catnames = $this->_commercant;
+        //create attribute option if do not exist
+        echo '//CREATE PRODUCT ATTRIBUTES FOR EACH COMMERCANTS//</br>';
+        $arg_attribute = 'commercant';
         $attr_model = Mage::getModel('catalog/resource_eav_attribute');
         $attr = $attr_model->loadByCode('catalog_product', $arg_attribute);
         $attr_id = $attr->getAttributeId();
@@ -229,7 +382,8 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
         }
 
         //Create categories and attributes option
-          $storeid = intval(Mage::getConfig()->getNode('stores')->{$code}->{'system'}->{'store'}->{'id'});
+        echo '//CREATE COMMERCANTS CATEGORIES//</br>';
+        $storeid = intval(Mage::getConfig()->getNode('stores')->{$this->_codeboutique}->{'system'}->{'store'}->{'id'});
         $rootCategoryId = Mage::app()->getStore($storeid)->getRootCategoryId();
 
         $oAttribute = Mage::getSingleton('eav/config')->getAttribute('catalog_category', 'att_com_id')->getSource()->getAllOptions();
@@ -250,7 +404,7 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
                 ->addAttributeToFilter('name', $childcat)
                 ->getFirstItem();
             if (null == $childCategory->getId()) {
-                // id of parent category
+                //id of parent category
                 $parentId = $parentCategory->getId();
                 $category = Mage::getModel('catalog/category');
                 $category->setName($childcat);
@@ -272,8 +426,9 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
     //Activation des catégories produits commerçants
     public function activatecatAction()
     {
-        $code = $this->_codeboutique;
-        $storeid = intval(Mage::getConfig()->getNode('stores')->{$code}->{'system'}->{'store'}->{'id'});
+        echo '//// Activer toutes les catégories en cliquable ////</br></br>';
+
+        $storeid = intval(Mage::getConfig()->getNode('stores')->{$this->_codeboutique}->{'system'}->{'store'}->{'id'});
         $categories = Mage::getModel('catalog/category')
                 ->getCollection()
                 ->addFieldToFilter('is_active', array('eq' => 1))
@@ -281,9 +436,15 @@ class Pmainguet_Commercants_IndexController extends Mage_Core_Controller_Front_A
         $rootCategoryId = Mage::app()->getStore($storeid)->getRootCategoryId();
         $categories->addAttributeToFilter('path', array('like' => '1/'.$rootCategoryId.'/%'));
         foreach ($categories as $cat) {
-            $cat->setIsClickable(true);
+            if(in_array($cat->getName(),array_keys($this->_commercant))){
+                $cat->setIsClickable("Non");
+                echo 'Catégorie '.$cat->getName().' non cliquable.</br>';
+            } else {
+                $cat->setIsClickable(true);
+                echo 'Catégorie '.$cat->getName().' cliquable.</br>';
+            }
             $cat->save();
         }
-        echo 'Catégories de '.$code.' activées!</br>';
+        echo 'Catégories de '.$this->_codeboutique.' activées!</br>';
     }
 }
