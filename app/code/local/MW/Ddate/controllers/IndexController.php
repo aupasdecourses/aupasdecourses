c9c0e43a756f114b91515582e310b620b312e305
pmainguet <mainguetpierre@gmail.com>
2016-05-10 11:06:04 +0200

    test Ddate*

--- app/code/local/MW/Ddate/controllers/IndexController.php
+++ app/code/local/MW/Ddate/controllers/IndexController.php
@@ -3,6 +3,6 @@
 class MW_Ddate_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function testAction(){
-        print_r(Mage::getSingleton('core/date')->timestamp());
+        print_r(date("d F Y H:i:s", Mage::getSingleton('core/date')->timestamp()));
     }
 }
