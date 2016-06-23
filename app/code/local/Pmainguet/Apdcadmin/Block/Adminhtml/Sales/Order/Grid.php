ffd82e589da1623433fa76ac2e7b689a4cbf2377
pmainguet <mainguetpierre@gmail.com>
2016-06-23 11:27:58 +0200

    Fix filtering in admin order grid + change title in landing page

--- app/code/local/Pmainguet/Apdcadmin/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/local/Pmainguet/Apdcadmin/Block/Adminhtml/Sales/Order/Grid.php
@@ -41,8 +41,7 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
         // //Amasty Order Attributes & Attachments are integrated via observer
 
         //MWDdate
-
-        $ddate      = Mage::getModel('ddate/dtime')->getCollection();
+        $ddate = Mage::getModel('ddate/dtime')->getCollection();
         $collection = Mage::getResourceModel($this->_getCollectionClass());
         $collection->getSelect()
         ->joinLeft(array(
@@ -63,22 +62,19 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
 
         //Address
          $collection->getSelect()->joinLeft('sales_flat_order_address', 'main_table.entity_id = sales_flat_order_address.parent_id AND sales_flat_order_address.address_type = "shipping"',array('postcode','street'));
-
-        $this->setCollection($collection);
+         $this->setCollection($collection);
 
         //Important to not use parent::_prepareCollection() but Mage_Adminhtml_Block_Widget_Grid (grandparent function)
         return Mage_Adminhtml_Block_Widget_Grid::_prepareCollection();
     }
 
     protected function _prepareColumns()
-
     {
-
         $this->addColumn('real_order_id', array(
             'header' => Mage::helper('sales')->__('Order #'),
             //'width' => '80px',
             'type' => 'text',
-            'index' => 'increment_id'
+            'index' => 'increment_id',
         ));
         
         if (!Mage::app()->isSingleStoreMode()) {
@@ -101,9 +97,10 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
         $this->addColumn('status', array(
             'header' => Mage::helper('sales')->__('Status'),
             'index' => 'status',
-            'type' => 'options',
+            'type'  => 'options',
             'width' => '70px',
-            'options' => Mage::getSingleton('sales/order_config')->getStatuses()
+            'options' => Mage::getSingleton('sales/order_config')->getStatuses(),
+            'filter_index' => 'main_table.status'
         ));
         
         $this->addColumn('billing_name', array(
@@ -119,11 +116,13 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
         $this->addColumn('street', array(
             'header' => Mage::helper('sales')->__('Adresse liv.'),
             'index' => 'street',
+            'filter_index' => 'sales_flat_order_address.street'
         ));
 
         $this->addColumn('postcode', array(
             'header' => Mage::helper('sales')->__('Arr.'),
             'index' => 'postcode',
+            'filter_index' => 'sales_flat_order_address.postcode'
         ));
 
         $this->addColumn('ddate', array(
@@ -143,8 +142,10 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
             'align' => 'center',
             'width' => '60px',
             'index' => 'dtimetext',
-            'type' => 'text'
+            'type' => 'text',
+            'filter_index' => 'mwddate.dtimetext'
         ));
+
         
         // $this->addColumn('base_grand_total', array(
         //     'header' => Mage::helper('sales')->__('G.T. (Base)'),
@@ -173,6 +174,18 @@ class Pmainguet_Apdcadmin_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtm
         $this->addExportType('*/*/exportExcel', Mage::helper('sales')->__('Excel'));
         
         //return parent::_prepareColumns();
+    }
+
+    protected function _myDdateFilter($collection, $column)
+    {
+        if (!$value = $column->getFilter()->getValue()) {
+            return $this;
+        }
+        $from = Mage::getSingleton('core/date')->gmtDate('Y-m-d', $value['orig_from']);
+        $to   = Mage::getSingleton('core/date')->gmtDate('Y-m-d', $value['orig_to']);
+        $this->getCollection()->getSelect()->where("mwddate.ddate >= '" . $from . "' AND ddate <= '" . $to . "'");
+        
+        return $this;
     }
     
 }
