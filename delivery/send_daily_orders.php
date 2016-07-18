9b70d60779d54e6900ccc5ec1f1c6f0955dadc5f
bhivert <hivert.benoit@gmail.com>
2016-07-18 22:54:38 +0200

    some constant values set in variables

--- delivery/send_daily_orders.php
+++ delivery/send_daily_orders.php
@@ -45,13 +45,13 @@ function getOrders($id, $date = TODAY_DATE) {
 	$orderObjs = orders_fortheday($date, $id);
 	$orders = [];
 	foreach ($orderObjs as $orderObj) {
-		$data['Commande n°'] = $orderObj->getData('increment_id');
-		$data['Prenom'] = $orderObj->getShippingAddress()->getData('firstname');
-		$data['Nom'] = $orderObj->getShippingAddress()->getData('lastname');
-		$data['Date prise de commande'] = $orderObj->getData('created_at');
-		$data['Date de livraison'] = (TODAY_DATE <= AMASTY_MW_DATE) ? $orderObj->getData('delivery_date') : $orderObj->getData('ddate');
-		$data['Heure de livraison'] = (TODAY_DATE <= AMASTY_MW_DATE) ? $orderObj->getData('delivery_time') : $orderObj->getData('dtime');
-		$data['Remplacement pour produit équivalent possible'] = $orderObj->getData('produit_equivalent');
+		$data['id'] = $orderObj->getData('increment_id');
+		$data['first_name'] = $orderObj->getShippingAddress()->getData('firstname');
+		$data['last_name'] = $orderObj->getShippingAddress()->getData('lastname');
+		$data['order_date'] = $orderObj->getData('created_at');
+		$data['delivery_date'] = (TODAY_DATE <= AMASTY_MW_DATE) ? $orderObj->getData('delivery_date') : $orderObj->getData('ddate');
+		$data['delivery_time'] = (TODAY_DATE <= AMASTY_MW_DATE) ? $orderObj->getData('delivery_time') : $orderObj->getData('dtime');
+		$data['equivalent_replacement'] = $orderObj->getData('produit_equivalent');
 		$data['Total quantité'] = 0;
 		$data['Total prix'] = 0.0;
 		$data['Order products'] = [];
@@ -63,20 +63,20 @@ function getOrders($id, $date = TODAY_DATE) {
 				$prod_data = [
 					'title'			=>	$oprod->getName(),
 					'prix_kilo'		=>	($oprod->getPrixKiloSite() <> "") ? $oprod->getPrixKiloSite() : $sprod->getPrixKiloSite(),
-					'quantite'		=>	round($oprod->getQtyOrdered(),0),
+					'quantite'		=>	round($oprod->getQtyOrdered(), 0),
 					'description'	=>	($oprod->getShortDescription() <> "") ? $oprod->getShortDescription() : $sprod->getShortDescription(),
 					'prix_unitaire'	=>	round($oprod->getPriceInclTax(),2),
 					'prix_total'	=>	round($oprod->getRowTotalInclTax(),2)
 					];
-				$prod_data['comment']	=	'';
+				$prod_data['comment'] = '';
 				$opts = $oprod->getProductOptions()['options'];
 				foreach ($opts as $opt) {
-					$prod_data['comment']	.=	$opt['label'] . ' ' . $opt['value'] . PHP_EOL;
+					$prod_data['comment'] .= $opt['label'] . ' ' . $opt['value'] . PHP_EOL;
 				}
-				$prod_data['comment']	.= $oprod->getData('item_comment') . PHP_EOL;
-				$data['Total quantité'] +=	$prod_data['quantite'];
-				$data['Total prix']		+=	$prod_data['prix_total'];
-				$data['Order products'][] = $prod_data;
+				$prod_data['comment']		.= $oprod->getData('item_comment') . PHP_EOL;
+				$data['Total quantité']		+= $prod_data['quantite'];
+				$data['Total prix']			+= $prod_data['prix_total'];
+				$data['Order products'][]	= $prod_data;
 			}
 		}
 		$orders[] = $data;
@@ -92,9 +92,14 @@ foreach ($commercants as $commercant_id => $commercant_info) {
 } 
 
 function generate_Pdf($commercant, $orders_date) {
-	print_r($commercant);
-	$lineHeight = 20;
+	$lineHeight_summary = 20;
 	$lineOffset_summary = 5;
+	$lineHeight_order = 15;
+	$lineOffset_order = 5;
+	$alignLeft = 50;
+	
+	print_r($commercant);	// Debug
+
 	$pdf = new Zend_Pdf();
 
 	//	Orders_Summary	==>>
@@ -105,43 +110,40 @@ function generate_Pdf($commercant, $orders_date) {
 
 	$orders_summary->setFillColor(new Zend_Pdf_Color_Rgb(0, 0, 0));
 	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 16);
-	$orders_summary->drawText('Commandes AU PAS DE COURSES', 50, $height - ($lineHeight * $lineOffset_summary++));
+	$orders_summary->drawText('Commandes AU PAS DE COURSES', $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 12);
-	$orders_summary->drawText('A ' . $commercant['name'] . ' pour le ' . $orders_date, 50, $height - ($lineHeight * $lineOffset_summary++));
+	$orders_summary->drawText("A {$commercant['name']} pour le {$orders_date}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	$lineOffset_summary++;
-	$orders_summary->drawText('Nombre de commandes: ' . count($commercant['orders']), 50, $height - ($lineHeight * $lineOffset_summary++));
+	$orders_summary->drawText('Nombre de commandes: ' . count($commercant['orders']), $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	//	<<==	Orders_Summary
 
 	$order_count = 0;
 	foreach ($commercant['orders'] as $order) {
-		$orders_summary->drawText('Commande n°' . ++$order_count . ': ' . $order['Commande n°'], 50, $height - ($lineHeight * $lineOffset_summary++));
+		$orders_summary->drawText("Commande n°" . ++$order_count . ": {$order['id']}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	//	Order	==>>
 		$orders_page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4_LANDSCAPE);
 		$pdf->pages[] = $orders_page;
 
-		$lineHeight = 15;
-		$lineOffset_order = 5;
-		
 		$orders_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 8);
-		$orders_page->drawText('Commande Au Pas De Courses - ' . $commercant['name'] . ' pour le ' . $orders_date, 50, $height - 45);
+		$orders_page->drawText("Commande Au Pas De Courses - {$commercant['name']} pour le {$orders_date}", $alignLeft, $height - 45);
 		$orders_page->setLineWidth(0.5);
-		$orders_page->drawLine(50, 50, $width - 50, 50);
+		$orders_page->drawLine($alignLeft, $height - 50, $width - $alignLeft, $height - 50);
 
 		$orders_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 16);
-		$orders_page->drawText('Commande n°  ' . $order['Commande n°'], 50, $height - ($lineHeight * $lineOffset_order++));
-		$lineOffset_order++;
+		$orders_page->drawText("Commande n°  {$order['id']}", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
 		$orders_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 12);
-		$orders_page->drawText('Nom du client: ' . $order['Prenom'] . ' ' . $order['Nom'], 50, $height - ($lineHeight * $lineOffset_order++));
-		$orders_page->drawText('Date de Prise de Commande: ' . $order['Date prise de commande'], 50, $height - ($lineHeight * $lineOffset_order++));
-		$orders_page->drawText('Date de Livraison: ' . $order['Date de livraison'], 50, $height - ($lineHeight * $lineOffset_order++));
-		$orders_page->drawText('Creneau de Livraison: ' . $order['Heure de livraison'], 50, $height - ($lineHeight * $lineOffset_order++));
-		$orders_page->drawText('Remplacement equivalent: ' . (($order['Remplacement pour produit équivalent possible']) ? 'oui' : 'non'), 50, $height - ($lineHeight * $lineOffset_order++));
-		$orders_page->drawText('Liste des produits commandes: ', 50, $height - ($lineHeight * $lineOffset_order++));
+		$orders_page->drawText("Nom du client: {$order['first_name']} {$order['last_name']}", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
+		$orders_page->drawText("Date de Prise de Commande: {$order['order_date']}", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
+		$orders_page->drawText("Date de Livraison: {$order['delivery_date']}", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
+		$orders_page->drawText("Creneau de Livraison: {$order['delivery_time']}", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
+		$orders_page->drawText("Remplacement equivalent: " . (($order['equivalent_replacement']) ? "oui" : "non"), $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
+		$orders_page->drawText("Liste des produits commandes: ", $alignLeft, $height - ($lineHeight_order * $lineOffset_order++));
 
+		//	<<===
 
-		$orders_page->drawLine(50, $height - 50, $width - 50, $height - 50);
+		$orders_page->drawLine($alignLeft, 50, $width - $alignLeft, 50);
 		$orders_page->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 8);
-		$orders_page->drawText('Genere le: ' . date('r'), 50, 40);
+		$orders_page->drawText("Genere le: " . date('r'), $alignLeft, 40);
 	//	<<==	Order
 	}
 
