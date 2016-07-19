cf4dc1a404daa11d2f4f6bec8c311b83530784d6
pmainguet <mainguetpierre@gmail.com>
2016-07-19 12:50:25 +0200

    Coupon stat

--- delivery/modules/clients/view.php
+++ delivery/modules/clients/view.php
@@ -1,14 +1,13 @@
 <?php
 // Vérification des droits d'accès de la page
 if (utilisateur_est_connecte()) {
-	// Affichage de la liste des commerçants
-	if($_GET['option']=='fidelity'){
-		include 'views/clients_fidelity.phtml';		
-	} elseif($_GET['option']=='stat') {
-		//$orders=liste_commande();
-		include 'views/clients_stat.phtml';
-	}
-	
+	$index=['fidelity'=>'views/clients_fidelity.phtml',
+		'stat'=>'views/clients_stat.phtml',
+		'coupon'=>'views/clients_coupon.phtml'
+	];
+	if(array_key_exists($_GET['option'], $index)){
+		include $index[$_GET['option']];
+	}	
 } else {
   // Inclusion de la vue d'erreur en cas de non connexion
   include CHEMIN_VUE_GLOBALE.'erreur_connexion.phtml';
