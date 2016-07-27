21edf3316323812ad9e523734acea13f2423f23f
bhivert <hivert.benoit@gmail.com>
2016-07-27 16:51:09 +0200

    little fix on summary offset when new page are created

--- delivery/models/magento.php
+++ delivery/models/magento.php
@@ -122,7 +122,7 @@ function all_orders($var = 'mwddate', $commercantId = 'all')
 {
     try {
         $orders = Mage::getModel('sales/order')->getCollection();
-        $from_date = date('Y-m-d H:i:s', mktime(0, 0, 0, date('m') - 1, date('d'), date('Y')));
+        $from_date = date('Y-m-d H:i:s', mktime(0, 0, 0, date('m') - 3, date('d'), date('Y')));
 
         //Ajout de MWDDate (pour les commandes après le 12 janvier environ)
         if ($var == 'mwddate') {
@@ -171,16 +171,8 @@ function orders_fortheday($date, $commercantId = 'all', $var = 'mwddate')
     $date1 = date('Y-m-d H:i:s', mktime(0, 0, 0, intval($d[1]), intval($d[2]) + 1, intval($d[0])));
     if ($var == 'mwddate') {
         $orders->addAttributeToFilter('ddate', array('eq' => $date));
-
-//        $orders->addAttributeToFilter('ddate', array(
-//                                'from' => $date,
-//                                'to' => $date1,
-//                        ));
     } else {
-        $orders->addAttributeToFilter('delivery_date', array(
-                                'from' => $date,
-                                'to' => $date1,
-                        ));
+        $orders->addAttributeToFilter('delivery_date', array('eq' => $date));
     }
 
     return $orders;
--- delivery/send_daily_orders.php
+++ delivery/send_daily_orders.php
@@ -14,7 +14,15 @@ $GLOBALS['REFUND_ITEMS_INFO_ID_LIMIT']=2016000249;
 
 include CHEMIN_MODELE.'magento.php';
 connect_magento();
+/*
+$lib_path = realpath(dirname(__FILE__) . '../lib');
+set_include_path("$libpath:".get_include_path());
 
+require_once 'Zend/Loader.php';
+require_once 'Zend/Loader/Autoloader.php';
+
+Zend_Loader_Autoloader::getInstance();
+ */
 function getCommercant() {
 	$commercants = [];
     $categories = Mage::getModel('catalog/category')->getCollection()->addAttributeToSelect('*')->addIsActiveFilter()->addFieldToFilter('estcom_commercant', ['neq' => NULL]);
@@ -73,7 +81,7 @@ function getOrders(&$commercants, $date = TODAY_DATE) {
 
 $commercants = getCommercant();
 
-$orders_date = date('Y-m-d', strtotime('2016-06-24'));
+$orders_date = date('Y-m-d', strtotime('2016-06-21'));
 
 getOrders($commercants, $orders_date);
 
@@ -90,49 +98,104 @@ class generatePdf {
 
 	private static	$_width = 842;
 	private	static	$_height = 595;
+	private			$_format;
 
 	private			$_summary = [];
+	private			$_summary_id = 0;
+
+	private			$_summary_columnWidth;
+	private			$_summary_columnOffset;
+	private			$_summary_startColumnOffset = 0;
+	private			$_summary_maxColumnOffset = 3;
+
 	private			$_summary_lineHeight = 20;
-	private			$_summary_lineOffset = 5;
+	private			$_summary_startLineOffset = 9;
+	private			$_summary_maxLineOffset;
+	private			$_summary_lineOffset;
+
+	private			$_orders_count = 0;
 
 	private			$_margin_horizontal = 50;
 	private			$_margin_vertical = 50;
+
+	private			$_finalized = false;
 
 	public function __construct($commercant, $orders_date) {
 		$this->_commercant = $commercant;
 
 		$this->_pdf = new Zend_Pdf();
 
-		$this->_font = Zend_Pdf_Font::fontWisthName(Zend_Pdf_Font::FONT_HELVETICA);
-		$this->_font_bold = Zend_Pdf_Font::fontWisthName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
-		$this->_font_italic = Zend_Pdf_Font::fontWisthName(Zend_Pdf_Font::FONT_HELVETICA_ITALIC);
+		// Algorithm constants set ==>>
+		$this->_font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA);
+		$this->_font_bold = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
+		$this->_font_italic = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_ITALIC);
 
-		$this->_summary = $this->_pdf->newPage(static::$_width . ':' . static::$_height . ':');
+		$this->_summary_columnWidth = (static::$_width - ($this->_margin_horizontal * 2)) / number_format($this->_summary_maxColumnOffset, 2);
+		$this->_summary_columnOffset = $this->_summary_startColumnOffset;
+		$this->_summary_lineOffset = $this->_summary_startLineOffset;
+		$this->_summary_maxLineOffset = (static::$_height / $this->_summary_lineHeight) - 10;
 
-		$this->_summary->setFillColor(new Zend_Pdf_Color_Rgb(0, 0, 0));
-		$this->_summary->setFont($this->_font, 16);
-		$this->_summary->drawText('Commandes AU PAS DE COURSES', $this->_margin_horizontal, static::$_height - ($this->_summary_lineHeight * $this->_summary_lineOffset++));
-		$this->_summary->setFont($this->_font, 12);
-		$this->_summary->drawText("A {$this->_commercant['name']} pour le {$orders_date}", $this->_margin_horizontal, $this->_height - ($this->_summary_lineHeight * $this->_summary_lineOffset++));
-		$this->_summary_lineOffset++;
-		$this->_summary_lineOffset++;
+		$this->_format = static::$_width . ':' . static::$_height . ':';
+		// <<==
+
+		// create summary first page and draw header ==>>
+		$this->_summary[0] = $this->_pdf->newPage($this->_format);
+		$this->_summary[0]->setFillColor(new Zend_Pdf_Color_Rgb(0, 0, 0));
+		$this->_summary[0]->setFont($this->_font, 16);
+		$this->_summary[0]->drawText('Commandes AU PAS DE COURSES', $this->_margin_horizontal, static::$_height - ($this->_summary_lineHeight * 5));
+		$this->_summary[0]->setFont($this->_font, 12);
+		$this->_summary[0]->drawText("A {$this->_commercant['name']} pour le {$orders_date}", $this->_margin_horizontal, static::$_height - ($this->_summary_lineHeight * 6));
+		// <<==
 	}
 
 	private function _finalizePdf() {
-		$this->_summary->drawText('Nombre de commandes: ' . $this->_orders_count, $this->_margin_horizontal, $this->_height - ($this->_summary_lineHeight * 8));
+		$this->_summary[0]->drawText('Nombre de commandes: ' . $this->_orders_count, $this->_margin_horizontal, static::$_height - ($this->_summary_lineHeight * 8));
+		foreach ($this->_summary as $page) {
+			$this->_pdf->pages[] = $page;
+		}
+	}
+
+	private function addOrderToSummary($order) {
+		if (!($this->_summary_lineOffset < $this->_summary_maxLineOffset)) { // add column to currenti summary
+			if ($this->_summary_columnOffset < $this->_summary_maxColumnOffset) {
+				$this->_summary_columnOffset++;
+			} else { // add page to summary
+				$this->_summary_startOffset = 5;
+				$this->_summary_columnOffset = $this->_summary_startColumnOffset;
+				$this->summary_id++;
+				$this->_summary[$this->_summary_id] = $this->_pdf->newPage($this->_format);
+			}
+			// reinitialyse line offset to start position
+			$this->_summary_lineOffset = $this->_summary_startLineOffset;
+		}
+		// add current order to summary
+		$this->_summary[$this->_summary_id]->drawText("Commande n°" . ++$this->_orders_count . ": {$order['id']}", $this->_margin_horizontal, static::$_height - ($this->_summary_lineHeight * $this->_summary_lineOffset++));
 	}
 
 	public function addOrder($order) {
-		$this->_summary->drawText("Commande n°" . ++$this->orders_count . ": {$order['id']}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++)); // <<== make a function if page is full create new;
+		$this->addOrderToSummary($order);
 	}
 
 	public function save($filename) {
+		if (!$this->finalized) {
+			$this->_finalizePdf();
+		}
+		$this->_pdf->save($filename);
 	}
 
 	public function send($from, $to) {
+		if (!$this->finalized)
+			$this->_finalizePdf();
 	}
 }
 
+$test = new generatePdf($commercants['7'], $orders_date);
+foreach ($commercants['7']['orders'] as $order) {
+	$test->addOrder($order);
+}
+$test->save('test.pdf');
+
+/*
 function generate_Pdf($commercant, $orders_date) {
 	$lineHeight_summary = 20;
 	$lineOffset_summary = 5;
@@ -140,26 +203,26 @@ function generate_Pdf($commercant, $orders_date) {
 	$lineOffset_order = 5;
 	$alignLeft = 50;
 	
-//	$pdf = new Zend_Pdf();
+	$pdf = new Zend_Pdf();
 
 	//	Orders_Summary	==>>
-//	$orders_summary = $pdf->newPage(Zend_Pdf_Page::SIZE_A4_LANDSCAPE);
+	$orders_summary = $pdf->newPage(Zend_Pdf_Page::SIZE_A4_LANDSCAPE);
 	$pdf->pages[] = $orders_summary;
-//	$width = $orders_summary->getWidth();
-//	$height = $orders_summary->getHeight();
+	$width = $orders_summary->getWidth();
+	$height = $orders_summary->getHeight();
 
-//	$orders_summary->setFillColor(new Zend_Pdf_Color_Rgb(0, 0, 0));
-//	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 16);
-//	$orders_summary->drawText('Commandes AU PAS DE COURSES', $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
-//	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 12);
-//	$orders_summary->drawText("A {$commercant['name']} pour le {$orders_date}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
-//	$lineOffset_summary++;
-//	$orders_summary->drawText('Nombre de commandes: ' . count($commercant['orders']), $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
+	$orders_summary->setFillColor(new Zend_Pdf_Color_Rgb(0, 0, 0));
+	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 16);
+	$orders_summary->drawText('Commandes AU PAS DE COURSES', $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
+	$orders_summary->setFont(Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA), 12);
+	$orders_summary->drawText("A {$commercant['name']} pour le {$orders_date}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
+	$lineOffset_summary++;
+	$orders_summary->drawText('Nombre de commandes: ' . count($commercant['orders']), $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	//	<<==	Orders_Summary
 
 	$order_count = 0;
 	foreach ($commercant['orders'] as $order) {
-//		$orders_summary->drawText("Commande n°" . ++$order_count . ": {$order['id']}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
+		$orders_summary->drawText("Commande n°" . ++$order_count . ": {$order['id']}", $alignLeft, $height - ($lineHeight_summary * $lineOffset_summary++));
 	//	Order	==>>
 		$orders_page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4_LANDSCAPE);
 		$pdf->pages[] = $orders_page;
@@ -188,3 +251,6 @@ function generate_Pdf($commercant, $orders_date) {
 	}
 	return ($pdf);
 }
+
+generate_Pdf($commercants['7'], $orders_date)->save('test.pdf');
+*/
