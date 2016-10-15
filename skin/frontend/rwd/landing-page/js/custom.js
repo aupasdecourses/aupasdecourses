cee860352db3b6b5094233e4b9327ba1247eb905
Pierre Mainguet <mainguetpierre@gmail.com>
2016-10-15 13:55:55 +0200

    small changes in landing page + images)

--- skin/frontend/rwd/landing-page/js/custom.js
+++ skin/frontend/rwd/landing-page/js/custom.js
@@ -3,7 +3,7 @@
 =================================== */
 
 $j(document).ready(function() {$j("#carousel-how").owlCarousel({autoPlay: 6000,itemsCustom : [[0, 1],[768, 3]],stopOnHover:true});});
-$j(document).ready(function() {$j("#carousel-quartier").owlCarousel({autoPlay: 6000,itemsCustom : [[0, 1],[768, 2]],stopOnHover:true});});
+//$j(document).ready(function() {$j("#carousel-quartier").owlCarousel({autoPlay: 6000,itemsCustom : [[0, 1],[768, 2]],stopOnHover:true});});
 $j(document).ready(function() {$j("#carousel-price").owlCarousel({autoPlay: 6000,itemsCustom : [[0, 1],[768, 3]],stopOnHover:true});});
 $j(document).ready(function() {$j("#carousel-comments").owlCarousel({autoPlay: 6000,itemsCustom : [[0, 1],[768, 3]],stopOnHover:true});});
 
