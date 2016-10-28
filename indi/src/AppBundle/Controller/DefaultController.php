534bf6aca241463671fcaee1c41b53ba6bc2c02d
Benoit HIVERT <benoit@aupasdecourses.com>
2016-10-28 14:12:54 +0200

    model entity created

--- indi/src/AppBundle/Controller/DefaultController.php
+++ indi/src/AppBundle/Controller/DefaultController.php
@@ -6,22 +6,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-//use Symfony\Component\HttpFoundation\Response;
-
-//use AppBundle\Entity;
-
-//use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-//use Symfony\Component\Form\Extension\Core\Type\IntergerType;
-
 class DefaultController extends Controller
 {
 	public function indexAction(Request $request)
 	{
 		$id = new \AppBundle\Entity\OrderId();
 		$form = $this->createForm(\AppBundle\Form\OrderIdType::class, $id);
-//			->add('id', IntegerType::class)
-//			->add('Search', SubmitType::class)
-//			->getForm();
+
+$form->handleRequest($request);
 
 		return $this->render('home/index.html.twig', [
 			'form'	=>	$form->createView()
