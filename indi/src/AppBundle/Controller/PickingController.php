f949482dcfa6a2d72bb7c454f9eeb0e34fd055a3
Benoit HIVERT <benoit@aupasdecourses.com>
2016-11-02 20:53:17 +0100

    controller finished ???

--- indi/src/AppBundle/Controller/PickingController.php
+++ indi/src/AppBundle/Controller/PickingController.php
@@ -14,61 +14,43 @@ class PickingController extends Controller
 {
     public function indexAction(Request $request)
     {
-	$mage = \Magento::getInstance();
-	if (!$mage->isLogged())
-		return $this->redirectToRoute('userLogin');
-
-	$entity_fromto = new\AppBundle\Entity\FromTo();
-	$form_fromto = $this->createForm(\AppBundle\Form\FromTo::class, $entity_fromto);
-
-	$entity_id = new \AppBundle\Entity\OrderId();
-	$form_id = $this->createForm(\AppBundle\Form\OrderId::class, $entity_id);
-
-	$form_fromto->handleRequest($request);
-	$form_id->handleRequest($request);
-
-	if ($form_id->isValid()) {
-		return $this->redirectToRoute('pickingIndex', [
-			'id' =>$entity_id->id
-		]);
-	} else if ($form_fromto->isValid()) {
-		return $this->redirectToRoute('pickingAll', [
-			'from' => $entity_fromto->from
-		]);
-	}	return $this->render('picking/index.html.twig', [
-		'forms' => [
-			$form_fromto->createView(),
-			$form_id->createView()
-		]
-	]);
-    }
-
-    public function pickingAllAction(Request $request, $from)
-    {
-     		$mage = \Magento::getInstance();
+		$mage = \Magento::getInstance();
 		if (!$mage->isLogged())
 			return $this->redirectToRoute('userLogin');
 
-		$entity_fromto = new \AppBundle\Entity\FromTo();
-		$form_fromto = $this->createForm(\AppBundle\Form\FromTo::class, $entity_fromto, [
-			'action' => $this->generateUrl('pickingIndex'),
-		]);
-		$entity_id = new \AppBundle\Entity\OrderId();
-		$form_id = $this->createForm(\AppBundle\Form\OrderId::class, $entity_id, [
-			'action' => $this->generateUrl('pickingIndex'),
-		]);
+		$entity_from = new\AppBundle\Entity\From();
+		$form_from = $this->createForm(\AppBundle\Form\From::class, $entity_from);
 
-		$form_fromto->get('from')->setData($from);
+		$form_from->handleRequest($request);
+
+		if ($form_from->isValid()) {
+			return $this->redirectToRoute('pickingAll', [
+				'from' => $entity_from->from
+				]);
+		}
+
+		return $this->render('picking/index.html.twig', [
+			'forms' => [ $form_from->createView() ]
+		]);
+	}
+
+    public function pickingAllAction(Request $request, $from)
+    {
+		$mage = \Magento::getInstance();
+		if (!$mage->isLogged())
+			return $this->redirectToRoute('userLogin');
+
+		$entity_from = new \AppBundle\Entity\From();
+		$form_from = $this->createForm(\AppBundle\Form\From::class, $entity_from, [
+				'action' => $this->generateUrl('pickingIndex'),
+			]);
+
+		$form_from->get('from')->setData($from);
 
 		return $this->render('picking/all.html.twig', [
-			'forms' => [
-				$form_fromto->createView(),
-				$form_id->createView()
-			],
-			'orders' => $mage->getOrders($from)
-		]);
-
-
+				'forms' => [ $form_from->createView() ],
+				'stores' => $mage->getMerchantsOrdersbyStore(-1, $from)
+			]);
     }
 }
 
--- indi/src/AppBundle/Controller/ShippingController.php
+++ indi/src/AppBundle/Controller/ShippingController.php
@@ -12,12 +12,43 @@ class ShippingController extends Controller
 {
     public function indexAction(Request $request)
     {
-        return new Response('SHIPPING INDEX CONTROLER');
-    }
+		$mage = \Magento::getInstance();
+		if (!$mage->isLogged())
+			return $this->redirectToRoute('userLogin');
+
+		$entity_from = new\AppBundle\Entity\From();
+		$form_from = $this->createForm(\AppBundle\Form\From::class, $entity_from);
+
+		$form_from->handleRequest($request);
+
+		if ($form_from->isValid()) {
+			return $this->redirectToRoute('shippingAll', [
+				'from' => $entity_from->from
+				]);
+		}
+
+		return $this->render('shipping/index.html.twig', [
+			'forms' => [ $form_from->createView() ]
+		]);
+	}
 
     public function shippingAllAction(Request $request)
     {
-        return new Response('SHIPPING ALL CONTROLER');
+		$mage = \Magento::getInstance();
+		if (!$mage->isLogged())
+			return $this->redirectToRoute('userLogin');
+
+		$entity_from = new \AppBundle\Entity\From();
+		$form_from = $this->createForm(\AppBundle\Form\From::class, $entity_from, [
+				'action' => $this->generateUrl('pickingIndex'),
+			]);
+
+		$form_from->get('from')->setData($from);
+
+		return $this->render('shipping/all.html.twig', [
+				'forms' => [ $form_from->createView() ],
+				'stores' => $mage->getMerchantsOrdersbyStore(-1, $from)
+			]);
     }
 }
 
