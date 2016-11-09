90b1633912bd0e2f913cd6d9584e7fd9f5c1b1e4
sturquier <turquiersimon@hotmail.fr>
2016-11-09 15:20:22 +0100

    class Adyen

--- indi/library/Adyen.php
+++ indi/library/Adyen.php
@@ -1,6 +1,7 @@
 <?php
 
 class Adyen {
+	/* IP SERVOR 137.74.162.115/32	*/
 
 	private $_ch;
 
@@ -22,54 +23,77 @@ class Adyen {
 		curl_close($this->_ch);
 	}
 
-
-	public function refundCurlAction() {
+	public function refundCurlAction($merchantAccount, $value, $originalReference, $reference)
+	{
+		$refundTable = array(
+			"merchantAccount" => $merchantAccount,
+			"modificationAmount" => array(
+				"value" => $value,
+				"currency" => "EUR"
+			),
+			"originalReference" => $originalReference,
+			"reference" => $reference
+		);
+		$jsonRefundTable = json_encode($refundTable);
 
 		curl_setopt($this->_ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/servlet/Payment/v18/refund");
-		curl_setopt($this->_ch, CURLOPT_USERPWD,"");
-
-		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonRefund);
-
-		$result = curl_exec($this->_ch);
+		curl_setopt($this->_ch, CURLOPT_USERPWD,"ws_224975@Company.AuPasDeCourses:N@%n^IStD?2Xc4ISkc[>r@7<g");
+		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonRefundTable);
+		$refundResult = curl_exec($this->_ch);
 
 		if(curl_errno($this->_ch)){
 			print "Error: ". curl_error($this->_ch);
 		} else {
-			var_dump($result);
+			var_dump($refundResult);
 		}
-
 	}
 
-	public function payoutCurlStoreAction(
-		$amount = array($currency, $value),
-		$bank = array($iban, $ownerName, $countryCode),
-		$merchantAccount,
-		$recurring = array($contract),
-		$reference, $shopperEmail, $shopperReference																
-	)  
+	public function payoutCurlStoreAndConfirmAction($value, $iban, $ownerName, $merchantAccount, $reference, $shopperEmail, $shopperReference)  
 	{
+		$storeTable = array(
+			"amount" => array(
+				"currency" =>"EUR",
+				"value" => $value
+			),
+			"bank" => array(
+				"iban" => $iban,
+				"ownerName" => $ownerName,
+				"countryCode" => "FR"
+			),
+			"merchantAccount" => $merchantAccount,
+			"recurring" => array(
+				"contract" => "PAYOUT"
+			),
+			"reference" => $reference,
+			"shopperEmail" => $shopperEmail,
+			"shopperReference" => $shopperReference
+		);
+		$jsonStoreTable = json_encode($storeTable);
 
 		curl_setopt($this->_ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/servlet/Payout/v12/storeDetailAndSubmitThirdParty");
 		curl_setopt($this->_ch, CURLOPT_USERPWD, "storePayout_104791@Company.AuPasDeCourses:9GsnR!sm3]*w7>rh%^bHSd!@2");
-
-		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonStore);
-
-		$result = curl_exec($this->_ch);
+		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonStoreTable);
+		$storeResult = curl_exec($this->_ch);
 
 		if(curl_errno($this->_ch)) {
 			print "Error: ". curl_error($this->_ch);
 		} else {
-			var_dump($result);
+			var_dump($storeResult);
 		}
-		return ($id);
-	}
 
-	public function payoutCurlConfirmAction($id) {
+		/* CONFIRMATION DU PAYOUT */
+		$jsonDecoded = json_decode($storeResult, true);
+		$jsonDecoded["merchantAccount"] = $merchantAccount;
+		$jsonDecoded["originalReference"] = $jsonDecoded["pspReference"];
+		unset($jsonDecoded["pspReference"]);
+		unset($jsonDecoded["resultCode"]);
+
+		$jsonEncoded = json_encode($jsonDecoded);
 
 		curl_setopt($this->_ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/servlet/Payout/v12/confirmThirdParty");
 		curl_setopt($this->_ch, CURLOPT_USERPWD, "reviewPayout_092607@Company.AuPasDeCourses:BZYi/(p3h<BTA<v13At3@Dcj*");
 
-		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonConfirm);
+		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonEncoded);
 
 		$result = curl_exec($this->_ch);
 
@@ -79,20 +103,4 @@ class Adyen {
 			var_dump($result);
 		}
 	}
-	/*
-	public function payoutCurlDeclineAction() {
-
-		curl_setopt($this->_ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/servlet/Payout/v12/declineThirdParty");
-		curl_setopt($this->_ch, CURLOPT_USERPWD, "reviewPayout_092607@Company.AuPasDeCourses:BZYi/(p3h<BTA<v13At3@Dcj*");
-
-		curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $jsonDecline);
-
-		$result = curl_exec($this->_ch);
-
-		if(curl_errno($this->_ch)) {
-			print "Error: ". curl_error($this->_ch);
-		} else {
-			var_dump($result);
-		}
-	}*/
 }
