a66a40f36297332024dfe4be3ebb1983dd20db2a
sturquier <turquiersimon@hotmail.fr>
2016-11-10 18:04:04 +0100

    img script

--- facturation/imgTraitement.php
+++ facturation/imgTraitement.php
@@ -1,17 +1,19 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
 $directory = ("./attachments");
 
 $content = opendir($directory);
 $files = array();
-$folders = array();
 
 while(($file = readdir($content)) !== FALSE) {
 			if($file != '.' && $file != '..'){
 				
 				if(!is_dir($directory.'/'.$file)) {
 					$files[] = $file;
-				} else { $folders = $file;
 				}
 			}
 			}
@@ -20,8 +22,22 @@ while(($file = readdir($content)) !== FALSE) {
 if (!empty($files)){
 	sort($files);
 
-	foreach($files as $link) {
-		echo "{$directory}/{$link}<br/>".PHP_EOL;
+	foreach($files as $value) {
+
+		// image sans "_ticket"		
+		$link = preg_replace('/_ticket/','',$value);
+
+		// dossiers correspondants aux noms d'img
+		$folder = preg_replace('/.jpeg/','',$link);
+		
+
+		$folderName = ("./attachments/{$folder}");
+	//	mkdir($folderName, 0777, true);
+
+		rename("$value" , "$folderName/$folder.jpeg");
+
+//		echo "{$directory}/{$link}<br/>".PHP_EOL;
+//		echo "{$directory}/{$folder}<br/>".PHP_EOL;
 	}
 }
 ?>
