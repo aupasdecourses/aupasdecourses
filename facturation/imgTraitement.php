6a3ab324386fa3b3562a277ad73040ed6eb4c145
sturquier <turquiersimon@hotmail.fr>
2016-11-14 15:45:35 +0100

    implementation terminee script PHP images

--- facturation/imgTraitement.php
+++ facturation/imgTraitement.php
@@ -3,41 +3,50 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-
-$directory = ("./attachments");
-
+$directory = ("./attachmentsV2");
 $content = opendir($directory);
 $files = array();
 
-while(($file = readdir($content)) !== FALSE) {
-			if($file != '.' && $file != '..'){
-				
-				if(!is_dir($directory.'/'.$file)) {
-					$files[] = $file;
-				}
-			}
-			}
-		closedir($content);
+while(($file = readdir($content)) !== FALSE)
+{
+	if($file != '.' && $file != '..')
+	{
+		if(!is_dir($directory.'/'.$file)) 
+		{
+			$files[] = $file;
+		}
+	}
+}
+closedir($content);
 
-if (!empty($files)){
+
+if (!empty($files))
+{
 	sort($files);
-
-	foreach($files as $value) {
-
-		// image sans "_ticket"		
+	foreach($files as $value)
+   	{
+		/* $link == "2016XX.jpeg" OU "2016XX_remb.png" OU "PNG/PDF/JPG..." */
 		$link = preg_replace('/_ticket/','',$value);
 
-		// dossiers correspondants aux noms d'img
-		$folder = preg_replace('/.jpeg/','',$link);
-		
+		// $link == "2016XX.jpeg"
+		if(substr($link, -5) == ".jpeg")
+		{
+			// $folder == "2016XX"
+			$folder = preg_replace('/.jpeg/','',$link);
 
-		$folderName = ("./attachments/{$folder}");
-	//	mkdir($folderName, 0777, true);
+			// $folderRoute == "./attach/2016XX"
+			$folderRoute = ("./attachmentsV2/{$folder}");
 
-		rename("$value" , "$folderName/$folder.jpeg");
-
-//		echo "{$directory}/{$link}<br/>".PHP_EOL;
-//		echo "{$directory}/{$folder}<br/>".PHP_EOL;
+			$oldmask = umask(0);
+			mkdir($folderRoute, 0777, true);
+			umask($oldmask);
+				
+			// 2016XX/2016XX_ticket.jpeg --> 2016XX/2016XX.jpeg 
+			if(rename("./attachmentsV2/{$value}" , "$folderRoute/$link"))
+			{
+				echo("Creation du repertoire et déplacement de l'image effectués pour : ".$link."<br/>");
+			}
+		}
 	}
 }
 ?>
