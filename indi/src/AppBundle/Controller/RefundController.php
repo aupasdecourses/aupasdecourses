f76dfcd87a8517f5876ae76084a6ba6156c86a33
Benoit HIVERT <benoit@aupasdecourses.com>
2016-11-14 11:26:28 +0100

    upload initial comit

--- indi/src/AppBundle/Controller/RefundController.php
+++ indi/src/AppBundle/Controller/RefundController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 include_once 'Magento.php';
 
 class RefundController extends Controller
@@ -24,17 +27,18 @@ class RefundController extends Controller
 
 		if ($form_from->isValid())
 			return $this->redirectToRoute('refundIndex', [ 'from' => $entity_from->from ]);
-		if (isset($from))
-			$orders = $mage->getOrders($from);
-		else
-			$orders = $mage->getOrders();
+		if (!isset($from))
+			return $this->redirectToRoute('refundIndex', [ 'from' => date('Y-m-d') ]);
+
+		$form_from->get('from')->setData($from);
+
+		$orders = $mage->getOrders($from);
 		 
 		return $this->render('refund/index.html.twig', [
 			'user' => $_SESSION['delivery']['username'],
 			'forms' => [ $form_from->createView() ],
 			'orders' => $orders
 		]);
-
 	}
 
 	public function refundUploadAction(Request $request, $id)
@@ -45,11 +49,24 @@ class RefundController extends Controller
 
 		$order = $mage->getOrderByMerchants($id);
 
+		$entity_upload = new \AppBundle\Entity\Upload();
+		$form_upload = $this->createFormBuilder($entity_upload);
+
+		foreach ($order as $merchant_id => $data) {
+			$form_upload->add(preg_replace("/[^-a-zA-Z0-9:]| /", "_", $data['merchant']['name']), FileType::class, [
+				'label' => [ "Yolo"/*$data['merchant']['name']*/ ],
+				'attr'	=> [
+					'class'	=> 'form-control'
+					]
+			]);
+			// << =====
+		}
+
 		return $this->render('refund/upload.html.twig', [
 			'user' => $_SESSION['delivery']['username'],
+			'forms' => $form_upload->getForm()->createView(),
 			'order' => $order
 		]);		
-
 	}
 
 	public function refundAttachmentAction(Request $request, $id)
