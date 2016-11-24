ad80b5a114b9e578945357ee2b660407722d2f0d
Erwan INYZANT <erwan.inyzant@otogo.com>
2016-11-24 18:51:12 +0700

    NEW : Add custom option select and configurable product options + swatches on category view (product list)

---
+++ app/code/local/Apdc/Catalog/Block/Product/List/Availability.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the GardenMedia Mission Project 
+ * 
+ * @category Apdc
+ * @package  Catalog
+ * @author   Erwan INYZANT <erwan@garden-media.fr> 
+ * @license  All right reserved to Garden Media Studio VN Company Limited
+ * @link     http://www.garden-media.fr
+ */
+/**
+ * Apdc_Catalog_Block_Product_List_Availability 
+ * 
+ * @category Apdc
+ * @package  Catalog
+ * @uses     Mage
+ * @uses     Mage_Core_Block_Template
+ * @author   Erwan INYZANT <erwan@garden-media.fr> 
+ * @license  All right reserved to Garden Media Studio VN Company Limited
+ * @link     http://www.garden-media.fr
+ */
+class Apdc_Catalog_Block_Product_List_Availability extends Mage_Core_Block_Template
+{
+}
---
+++ skin/frontend/rwd/commis/js/apdc_catalog/category_view.js
@@ -0,0 +1,73 @@
+(function($) {
+  $(document).ready(function() {
+    if ($('.apdc-products-grid').length > 0) {
+        var alignProductGridActions = function () {
+            // Loop through each product grid on the page
+            $j('.apdc-products-grid').each(function(){
+                var gridRows = []; // This will store an array per row
+                var tempRow = [];
+                productGridElements = $j(this).children('li');
+                productGridElements.each(function (index) {
+                    // The JS ought to be agnostic of the specific CSS breakpoints, so we are dynamically checking to find
+                    // each row by grouping all cells (eg, li elements) up until we find an element that is cleared.
+                    // We are ignoring the first cell since it will always be cleared.
+                    if ($j(this).css('clear') !== 'none' && index !== 0) {
+                        gridRows.push(tempRow); // Add the previous set of rows to the main array
+                        tempRow = []; // Reset the array since we're on a new row
+                    }
+                    tempRow.push(this);
+
+                    // The last row will not contain any cells that clear that row, so we check to see if this is the last cell
+                    // in the grid, and if so, we add its row to the array
+                    if (productGridElements.length == index + 1) {
+                        gridRows.push(tempRow);
+                    }
+                });
+
+                $j.each(gridRows, function () {
+                    var tallestProductInfo = 0;
+                    $j.each(this, function () {
+                        // Since this function is called every time the page is resized, we need to remove the min-height
+                        // and bottom-padding so each cell can return to its natural size before being measured.
+                        $j(this).find('.product-info').css({
+                            'min-height': '',
+                            //'padding-bottom': ''
+                        });
+
+                        // We are checking the height of .product-info (rather than the entire li), because the images
+                        // will not be loaded when this JS is run.
+                        var productInfoHeight = $j(this).find('.product-info').height();
+                        // Space above .actions element
+                        var actionSpacing = 20;
+                        // The height of the absolutely positioned .actions element
+                        var actionHeight = $j(this).find('.product-info .actions').height();
+
+                        // Add height of two elements. This is necessary since .actions is absolutely positioned and won't
+                        // be included in the height of .product-info
+                        var totalHeight = productInfoHeight + actionSpacing + actionHeight;
+                        if (totalHeight > tallestProductInfo) {
+                            tallestProductInfo = totalHeight;
+                        }
+
+                        // Set the bottom-padding to accommodate the height of the .actions element. Note: if .actions
+                        // elements are of varying heights, they will not be aligned.
+                        //$j(this).find('.product-info').css('padding-bottom', actionHeight + 'px');
+                    });
+                    // Set the height of all .product-info elements in a row to the tallest height
+                    $j.each(this, function () {
+                        $j(this).find('.product-info').css('min-height', tallestProductInfo);
+                    });
+                });
+            });
+        };
+        alignProductGridActions();
+
+        // Since the height of each cell and the number of columns per page may change when the page is resized, we are
+        // going to run the alignment function each time the page is resized.
+        $j(window).on('delayed-resize', function (e, resizeEvent) {
+            alignProductGridActions();
+        });
+    }
+  });
+})(jQuery);
+
