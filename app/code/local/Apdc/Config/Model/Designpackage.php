d1c824ba3a3aa38fd63714d687b00e5fae317e45
Pierre Mainguet <mainguetpierre@gmail.com>
2016-11-24 11:16:22 +0100

    fix MergeCSS with timestamp on file name

---
+++ app/code/local/Apdc/Config/Model/Designpackage.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Apdc_Config_Model_Designpackage extends Mage_Core_Model_Design_Package
+{
+
+    /**
+     * Merge specified css files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedCssUrl($files)
+    {
+        // secure or unsecure
+        $isSecure = Mage::app()->getRequest()->isSecure();
+        $mergerDir = $isSecure ? 'css_secure' : 'css';
+        $targetDir = $this->_initMergerDir($mergerDir);
+        if (!$targetDir) {
+            return '';
+        }
+
+        // base hostname & port
+        $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
+        $hostname = parse_url($baseMediaUrl, PHP_URL_HOST);
+        $port = parse_url($baseMediaUrl, PHP_URL_PORT);
+        if (false === $port) {
+            $port = $isSecure ? 443 : 80;
+        }
+
+        // merge into target file
+        $targetFilename = md5(implode(',', $files) . "|{$hostname}|{$port}") .time().'.css';
+        $mergeFilesResult = $this->_mergeFiles(
+            $files, $targetDir . DS . $targetFilename,
+            false,
+            array($this, 'beforeMergeCss'),
+            'css'
+        );
+        if ($mergeFilesResult) {
+            return $baseMediaUrl . $mergerDir . '/' . $targetFilename;
+        }
+        return '';
+    }
+}
