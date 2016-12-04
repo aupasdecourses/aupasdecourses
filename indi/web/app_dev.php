9363a82fda3317b725e1d2ff92843e3eebb0eaa0
Pierre Mainguet <mainguetpierre@gmail.com>
2016-12-04 18:58:51 +0100

    integration branch indi

--- indi/web/app_dev.php
+++ indi/web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['62.161.8.215', '81.57.69.15', '92.170.172.23']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['62.161.8.215', '81.57.69.15', '92.170.172.23','127.0.0.1']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
