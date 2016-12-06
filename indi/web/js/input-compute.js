feaf3370e0bb3ad4162779ae42b1019fa86f7726
Benoit HIVERT <benoit@aupasdecourses.com>
2016-12-06 09:31:24 +0100

    up

--- indi/web/js/input-compute.js
+++ indi/web/js/input-compute.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-	function set_diff_color(elem, diff) {
-		$(this).removeClass('success warning error');
-		if (!diff)
-			elem.addClass('success');
-		else if (diff > 0)
-			elem.addClass('warning');
-		else
-			elem.addClass('error');
-	}
+function set_diff_color(elem, diff) {
+	$(this).removeClass('success warning error');
+	if (!diff)
+		elem.addClass('success');
+	else if (diff > 0)
+		elem.addClass('warning');
+	else
+		elem.addClass('error');
+}
 
-	var r_ticket = /([0-9]*)-diff/;
+$(document).ready(function() {
+	var r_diff = /([0-9]*)-diff/;
+	var r_ticket = /([0-9]*)-ticket/;
 
 	$('.merchant').each(function() {
 		var merchant_id = $(this).attr('id');
@@ -18,7 +19,7 @@ $(document).ready(function() {
 
 		function update_diff(merchant_id) {
 			$('#' + merchant_id + ' .diff').each(function() {
-				var item_id = r_ticket.exec($(this).attr('id'))[1];
+				var item_id = r_diff.exec($(this).attr('id'))[1];
 				somme_ticket += parseFloat($('#' + item_id + '-ticket').attr('value'));
 
 				var this_diff = parseFloat($(this).html());
@@ -28,24 +29,33 @@ $(document).ready(function() {
 			});
 		}
 
+		function update_diff() {
+
+		}
+
+		$('#' + merchant_id + ' .ticket-input').each(function() {
+			$(this).change(function () {
+				var item_id = r_ticket.exec($(this).attr('id'))[1];
+				var total_0 = parseFloat($('#' + merchant_id + '-total').html()).toFixed(2);
+				var total_1 = parseFloat($(this).attr('value')).toFixed(2);
+
+				somme_ticket = 0;
+				somme_diff = 0;
+
+				update_diff(merchant_id);
+
+				$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
+				$('#' + merchant_id + '-diff-total').html((total_0 - total_1).toFixed(2));
+
+				set_diff_color($('#' + merchant_id + '-diff'), somme_diff);
+			});
+		});
+
 		update_diff(merchant_id);
+
 		$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
 		$('#' + merchant_id + '-diff').html(somme_diff.toFixed(2));
 		set_diff_color($('#' + merchant_id + '-diff'), somme_diff);
 	});
 });
-	
 
-//	var r_ticket = /([0-9]*)\[ticket\]/;
-//
-//	$('.ticket').each(function(){
-//		var item_id = r_ticket.exec($(this).attr('id'))[1];
-//
-//		var name = '#'.concat(item_id.concat('-total'));
-//
-//		var total = $(name).html();
-//		var ticket = $(this).attr('value');
-//
-//		var diff = parseFloat(total) - parseFloat(ticket);
-//		$('#'.concat(item_id.concat('-diff'))).text(diff.toFixed(2));
-//	});
