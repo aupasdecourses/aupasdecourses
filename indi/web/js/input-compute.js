ab83592c175349fa622b4840892d050c8e8ee340
Benoit HIVERT <benoit@aupasdecourses.com>
2016-12-06 11:57:07 +0100

    up

--- indi/web/js/input-compute.js
+++ indi/web/js/input-compute.js
@@ -8,54 +8,91 @@ function set_diff_color(elem, diff) {
 		elem.addClass('error');
 }
 
-$(document).ready(function() {
-	var r_diff = /([0-9]*)-diff/;
-	var r_ticket = /([0-9]*)-ticket/;
-
-	$('.merchant').each(function() {
-		var merchant_id = $(this).attr('id');
+function init_diff(merchant_id) {
+	$('#' + merchant_id + ' .diff').each(function() {
+		var r_diff = /([0-9]*)-diff/;
 		var somme_ticket = 0;
 		var somme_diff = 0;
 
-		function update_diff(merchant_id) {
-			$('#' + merchant_id + ' .diff').each(function() {
-				var item_id = r_diff.exec($(this).attr('id'))[1];
-				somme_ticket += parseFloat($('#' + item_id + '-ticket').attr('value'));
+		var item_id = r_diff.exec($(this).attr('id'))[1];
+		somme_ticket += parseFloat($('#' + item_id + '-ticket').attr('value'));
 
-				var this_diff = parseFloat($(this).html());
-				somme_diff += this_diff;
+		var this_diff = parseFloat($(this).html());
+		somme_diff += this_diff;
 
-				set_diff_color($(this), this_diff);
-			});
-		}
+		set_diff_color($(this), this_diff);
 
-		function update_diff() {
+		$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
+		$('#' + merchant_id + '-diff-total').html(somme_diff.toFixed(2));
+		set_diff_color($('#' + merchant_id + '-diff-total'), somme_diff);
+	});
+}
 
-		}
+$(document).ready(function() {
+	$('.merchant').each(function() {
+		init_diff($(this).attr('id'));
 
 		$('#' + merchant_id + ' .ticket-input').each(function() {
-			$(this).change(function () {
-				var item_id = r_ticket.exec($(this).attr('id'))[1];
-				var total_0 = parseFloat($('#' + merchant_id + '-total').html()).toFixed(2);
-				var total_1 = parseFloat($(this).attr('value')).toFixed(2);
-
-				somme_ticket = 0;
-				somme_diff = 0;
-
-				update_diff(merchant_id);
-
-				$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
-				$('#' + merchant_id + '-diff-total').html((total_0 - total_1).toFixed(2));
-
-				set_diff_color($('#' + merchant_id + '-diff'), somme_diff);
+			$(this).on('change', function () {
+				// update {{product_id}}-ticket-total
+				// update {{product_id}}-diff
 			});
 		});
 
-		update_diff(merchant_id);
-
-		$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
-		$('#' + merchant_id + '-diff').html(somme_diff.toFixed(2));
-		set_diff_color($('#' + merchant_id + '-diff'), somme_diff);
+		$('#' + merchant_id + ' .diff').each(function() {
+			$(this).on('change', function () {
+				// update {{merchant_id}}-diff-total
+			});
+		});
 	});
 });
+
+//	$('.merchant').each(function() {
+//		var merchant_id = $(this).attr('id');
+//
+//		function init_diff(merchant_id) {
+//			$('#' + merchant_id + ' .diff').each(function() {
+//				var r_diff = /([0-9]*)-diff/;
+//				var somme_ticket = 0;
+//				var somme_diff = 0;
+//
+//				var item_id = r_diff.exec($(this).attr('id'))[1];
+//				somme_ticket += parseFloat($('#' + item_id + '-ticket').attr('value'));
+//
+//				var this_diff = parseFloat($(this).html());
+//				somme_diff += this_diff;
+//
+//				set_diff_color($(this), this_diff);
+//
+//				$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
+//				$('#' + merchant_id + '-diff-total').html(somme_diff.toFixed(2));
+//				set_diff_color($('#' + merchant_id + '-diff-total'), somme_diff);
+//			});
+//		}
+//
+//		function update_diff() {
+//
+//		}
+//
+//		$('#' + merchant_id + ' .ticket-input').each(function() {
+//			$(this).on('change', function () {
+//				var r_ticket = /([0-9]*)-ticket/;
+//				var merchant_id = $(this).closest("table").attr('id');
+//				var item_id = r_ticket.exec($(this).attr('id'))[1];
+//				var total_0 = parseFloat($('#' + merchant_id + '-total').html()).toFixed(2);
+//				var total_1 = parseFloat($(this).attr('value')).toFixed(2);
+//
+//				var somme_ticket = 0;
+//				var somme_diff = 0;
+//
+//				init_diff(merchant_id);
+//
+//				$('#' + merchant_id + '-ticket-total').html(somme_ticket.toFixed(2));
+//				$('#' + merchant_id + '-diff-total').html((total_0 - total_1).toFixed(2));
+//				set_diff_color($('#' + merchant_id + '-diff'), somme_diff);
+//			});
+//		});
+//
+//		init_diff(merchant_id);
+//	});
 
