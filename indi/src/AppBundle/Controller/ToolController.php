d0a59fb660cab20cb4195d6d1042cebd3f00fbb7
sturquier <turquiersimon@hotmail.fr>
2016-12-09 18:03:45 +0100

    Image catalogue up

--- indi/src/AppBundle/Controller/ToolController.php
+++ indi/src/AppBundle/Controller/ToolController.php
@@ -11,8 +11,6 @@ use AppBundle\Entity\Catalog;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-
 
 include_once 'Magento.php';
 
@@ -20,27 +18,12 @@ class ToolController extends Controller
 {
 	public function uploadAction(Request $request)
 	{
-		$catalog = array('' => '');
-
 		$mage = \Magento::getInstance();
 		if(!$mage->isLogged())
 			return $this->redirectToRoute('userLogin');
 
-		
-		$form = $this->createFormBuilder($catalog)
-			->add('File', FileType::class)
-			->getForm();
-
-		$form->handleRequest($request);
-
-		if ($form->isSubmitted() && $form->isValid())
-		{
-			$data = $form->getData();
-		}
-
 		return $this->render('tool/upload.html.twig', [
 			'user' => $_SESSION['delivery']['username'],
-			'form' => $form->createView(),
 		]);
 	}
 }
