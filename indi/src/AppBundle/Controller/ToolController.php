4568eed581a1771c94de84e37172871c86d40db5
sturquier <turquiersimon@hotmail.fr>
2016-12-09 13:10:47 +0100

    bundle upload many files first implement

--- indi/src/AppBundle/Controller/ToolController.php
+++ indi/src/AppBundle/Controller/ToolController.php
@@ -3,10 +3,16 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+use AppBundle\Entity\Catalog;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
 
 include_once 'Magento.php';
 
@@ -14,13 +20,27 @@ class ToolController extends Controller
 {
 	public function uploadAction(Request $request)
 	{
+		$catalog = array('' => '');
+
 		$mage = \Magento::getInstance();
 		if(!$mage->isLogged())
 			return $this->redirectToRoute('userLogin');
 
+		
+		$form = $this->createFormBuilder($catalog)
+			->add('File', FileType::class)
+			->getForm();
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid())
+		{
+			$data = $form->getData();
+		}
 
 		return $this->render('tool/upload.html.twig', [
-			'user' => $_SESSION['delivery']['username']
+			'user' => $_SESSION['delivery']['username'],
+			'form' => $form->createView(),
 		]);
 	}
 }
