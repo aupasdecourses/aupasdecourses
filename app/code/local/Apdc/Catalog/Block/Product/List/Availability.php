73cdde385bb60edbd78b3c8732e82d6c8243d4b6
Erwan INYZANT <erwan.inyzant@otogo.com>
2016-12-10 15:42:38 +0700

    NEW : add basic Availability informations on product list Hover

--- app/code/local/Apdc/Catalog/Block/Product/List/Availability.php
+++ app/code/local/Apdc/Catalog/Block/Product/List/Availability.php
@@ -22,4 +22,55 @@
  */
 class Apdc_Catalog_Block_Product_List_Availability extends Mage_Core_Block_Template
 {
+    /**
+     * getAvailability 
+     * 
+     * @param Mage_Catalog_Model_Product $product product 
+     * 
+     * @return array
+     */
+    public function getAvailability(Mage_Catalog_Model_Product $product)
+    {
+        return array(2,3,4,5);
+    }
+
+    /**
+     * getWeekDays 
+     * 
+     * @return array
+     */
+    public function getWeekDays()
+    {
+        //return Mage::getSingleton('adminhtml/system_config_source_locale_weekdays')->toOptionArray();
+        return array(
+            1 => array(
+                'value' => 1,
+                'label' => 'L'
+            ),
+            2 => array(
+                'value' => 2,
+                'label' => 'M'
+            ),
+            3 => array(
+                'value' => 3,
+                'label' => 'M'
+            ),
+            4 => array(
+                'value' => 4,
+                'label' => 'J'
+            ),
+            5 => array(
+                'value' => 5,
+                'label' => 'V'
+            ),
+            6 => array(
+                'value' => 6,
+                'label' => 'S'
+            ),
+            7 => array(
+                'value' => 0,
+                'label' => 'D'
+            )
+        );
+    }
 }
