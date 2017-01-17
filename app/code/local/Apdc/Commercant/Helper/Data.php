868fb2761b8fb0616eee822a1ac3d125793ab172
Pierre Mainguet <mainguetpierre@gmail.com>
2017-01-17 12:26:08 +0100

    add Availability on commercant page

--- app/code/local/Apdc/Commercant/Helper/Data.php
+++ app/code/local/Apdc/Commercant/Helper/Data.php
@@ -10,4 +10,19 @@ class Apdc_Commercant_Helper_Data extends Mage_Core_Helper_Abstract
         return ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
     }
 
+    public function formatDays($days, $string=false){
+    	$labeldays=$this->getDays();
+        $rsl=[];
+        foreach($days as $day){
+            $rsl[]=$labeldays[$day-1];
+        }
+
+        if ($string){
+           	$r=implode(", ", $rsl);
+        }else {
+        	$return=$rsl;
+        }
+        return $r;
+    }
+
 }
