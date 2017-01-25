36f7f199542c928f6a48661312732d7cff7dad15
sturquier <turquiersimon@hotmail.fr>
2017-01-25 13:55:33 +0100

    up

--- indi/src/Apdc/ApdcBundle/Controller/BillingController.php
+++ indi/src/Apdc/ApdcBundle/Controller/BillingController.php
@@ -18,6 +18,32 @@ class BillingController extends Controller
 
 		$mage = $this->container->get('apdc_apdc.magento');
 
-		return $this->render('ApdcApdcBundle::billing/index.html.twig');
+		$entity_fromto = new \Apdc\ApdcBundle\Entity\FromTo();
+		$form_fromto = $this->createForm(\Apdc\ApdcBundle\Form\FromTo::class, $entity_fromto);
+
+		$form_fromto->handleRequest($request);
+
+		if($form_fromto->isSubmitted() && $form_fromto->isValid())
+		{
+			return $this->redirectToRoute('billingAll', [
+				'from'	=> $entity_fromto->from,
+				'to'	=> $entity_fromto->to
+			]);
+		}
+
+		return $this->render('ApdcApdcBundle::billing/index.html.twig', [
+			'forms'	=> [
+				$form_fromto->createView()
+			]
+		]);
+	}
+
+	public function billingAllAction(Request $request, $from, $to)
+	{
+		$mage = $this->container->get('apdc_apdc.magento');
+
+		return $this->render('ApdcApdcBundle::billing/all.html.twig', [
+			'orders' => $mage->getOrders($from, $to)
+		]);
 	}
 }
