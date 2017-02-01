016b6d19782d494178a19b924b7aeaf53e7d86de
Pierre Mainguet <mainguetpierre@gmail.com>
2017-02-01 08:18:44 +0100

    change in credit memo mail template

--- delivery/libs/processimage/script.php
+++ delivery/libs/processimage/script.php
@@ -1,16 +1,16 @@
 <?php
 if($_POST['action']=='optimiser'){
-	shell_exec ("./photos_produits.sh");
+	shell_exec ("bash photos_produits.sh");
 	echo "Processing OK!";
 }elseif($_POST['action']=='reindex'){
-	shell_exec ("./apdc-reindex-all.sh");
+	shell_exec ("bash apdc-reindex-all.sh");
 	echo "Reindex OK!";
 }
 elseif($_POST['action']=='commercants'){
-	shell_exec ("./photos_commercants.sh ".$_POST['image']);
+	shell_exec (" bash photos_commercants.sh ".$_POST['image']);
 	echo "Optimisation and thumbnail creation done!";
 }elseif($_POST['action']=='categories'){
-	shell_exec ("./photos_categories.sh ".$_POST['image']);
+	shell_exec ("bash photos_categories.sh ".$_POST['image']);
 	echo "Optimisation and thumbnail creation done!";
 }else{
 	echo "error";
