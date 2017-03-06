50cafb6a12fb0234664c8acd75ffef458ebe54d4
sturquier <turquiersimon@hotmail.fr>
2017-03-06 17:56:14 +0100

    standard Forms

--- indi/src/Apdc/ApdcBundle/Form/FromTo.php
+++ indi/src/Apdc/ApdcBundle/Form/FromTo.php
@@ -5,7 +5,6 @@ namespace Apdc\ApdcBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,17 +13,17 @@ class FromTo extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder->add('from', TextType::class, [
-			'required' => true,
-			'label' => 'Date début:',
-			'attr' => [
-				'class' => 'form-control datepicker'
+			'required'	=> true,
+			'label'		=> 'Date début:',
+			'attr'		=> [
+				'class'		=> 'form-control datepicker'
 			]
 		]);
 		$builder->add('to', TextType::class, [
-			'required' => true,
-			'label' => 'Date fin:',
-			'attr' => [
-				'class' => 'form-control datepicker'
+			'required'	=> true,
+			'label'		=> 'Date fin:',
+			'attr'		=> [
+				'class'		=> 'form-control datepicker'
 			]
 		]);
 		$builder->add('Search', SubmitType::class);
