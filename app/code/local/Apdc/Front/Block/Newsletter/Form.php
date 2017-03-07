df4fcfeeac47a3a5715aea10fdabe7155ba58730
Pierre Mainguet <mainguetpierre@gmail.com>
2017-03-07 19:48:04 +0100

    finalize newsletter popup

--- app/code/local/Apdc/Front/Block/Newsletter/Form.php
+++ app/code/local/Apdc/Front/Block/Newsletter/Form.php
@@ -3,8 +3,56 @@
 class Apdc_Front_Block_Newsletter_Form extends Ebizmarts_MageMonkey_Block_Lists
 {
 
+	private $_listcustom=[
+			"Paris 1er",
+			"Paris 2e",
+			"Paris 3e",
+			"Paris 4e",
+			"Paris 5e",
+			"Paris 6e",
+			"Paris 7e",
+			"Paris 8e",
+			"Paris 9e",
+			"Paris 10e",
+			"Paris 11e",
+			"Paris 12e",
+			"Paris 13e",
+			"Paris 14e",
+			"Paris 15e",
+			"Paris 16e",
+			"Paris 17e",
+			"Paris 18e",
+			"Paris 19e",
+			"Paris 20e",
+			"Levallois Perret",
+			"Neuilly sur Seine",
+			"Boulogne Billancourt",
+			"Vincennes",
+			"Issy-les-Moulineaux",
+		];
+
 	public function getNewsletterlist(){
-		return $this->getLists();
+		
+		$mclists=$this->getLists();
+		$mclists_size=count($mclists);
+		$arr=array();
+		
+		foreach($this->_listcustom as $q){
+			foreach($mclists as $key=>$ql){
+				if($ql['name']==$q){
+					$arr[$q]=$ql['id'];
+					unset($mclists[$key]);
+					break;
+				}
+			}
+		}
+
+		if($mclists_size<>count($arr)){
+			Mage::log('Newsletter entry missing on landing page newsletter form',null,'Ebizmart_Newsletter_Form.log');
+		}
+
+		return $arr;
+
 	}
 
 }
