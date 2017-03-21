c0406ec9b42293c32a1b3e37edb96304bed80f51
sturquier <turquiersimon@hotmail.fr>
2017-03-21 17:31:56 +0100

    up before pull apdc/apdc for apdc_notation table

--- indi/src/Apdc/ApdcBundle/Controller/StatController.php
+++ indi/src/Apdc/ApdcBundle/Controller/StatController.php
@@ -75,9 +75,20 @@ class StatController extends Controller
 		if (!$this->isGranted('ROLE_ADMIN')) {
 			return $this->redirectToRoute('root');
 		}
+		
+		$stats = $this->container->get('apdc_apdc.stats');
 
+		if (isset($_GET['date_debut'])) {
+			$date_debut	= $_GET['date_debut'];
+			$date_fin	= $stats->end_month($date_debut);
+		}
+
+//		$notes = $stats->getNotes($date_debut, $date_fin);
+		
 		return $this->render('ApdcApdcBundle::stat/rateOrder.html.twig', [
-
+			'date_debut'	=> $date_debut,
+			'date_fin'		=> $date_fin,
+//			'notes'			=> $notes,
 		]);
 	}
 }
--- indi/src/Apdc/ApdcBundle/Services/Stats.php
+++ indi/src/Apdc/ApdcBundle/Services/Stats.php
@@ -225,11 +225,46 @@ class Stats
 
 
 
+	/****************************
+	 * STATS CLIENTS *****************/
 
 
+	public function end_month($date) 
+	{
+		$date = strtotime('+1 month', strtotime(str_replace('/', '-', $date)));
+		$date = strtotime('-1 second', $date);
+		$date = date('Y-m-d', $date);
+
+		return $date;
+	}
 
 
+	public function getNotes($date_debut, $date_fin)
+	{
+		$notationClient = \Mage::getModel('apdc_notation/notation')->getCollection();
+		$result = array();
+		foreach ($notationClient as $n) {
+			$result[] = array(
+				'order_id'	=> $n->getOrderId(),
+				'note'		=> $n->getNote(),
+			);
+		}
 
+		return $result;
+	}
 
+	public function histogramme()
+	{
+		$notes = $this->getNotes();
+		$result = array();
+		foreach ($notes as $id => $n) {
+			if (!array_key_exists($n, $result)) {
+				$result[$n] = 1;
+			} else {
+				$result[$n] += 1;
+			}
+		}
 
+		return json_encode($result);
+	}
 }
