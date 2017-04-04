a890409b6aee97444f7b7cda7dcfaec2fbfcaeab
nbaumont <n.baumont@touchfordiffusion.com>
2017-04-04 21:08:20 +0200

    Corrections JS

---
+++ app/code/local/Apdc/Blog/Block/Template.php
@@ -0,0 +1,63 @@
+<?php
+
+class Apdc_Commercant_Block_List extends Mage_Catalog_Block_Product
+{
+    public function getListShops()
+    {
+        //Fonction peut être trop complexe: suffit de filter les catégories de la boutique à afficher, pas besoin de vérifier si le commerçant est activé ou non.
+        $storeid = Mage::app()->getStore()->getId();
+        $rootId = Mage::app()->getStore($storeid)->getRootCategoryId();
+        $filter = array();
+        $data = array();
+
+        $filter=Mage::helper('apdc_commercant')->getCategoriesInfos($rootId);
+
+        $shops = Mage::getModel('apdc_commercant/shop')->getCollection()
+            ->addFieldToFilter('stores', array('finset' =>$storeid))
+            ->addFieldtoFilter('enabled',1);
+
+        foreach ($shops as $shop) {
+            $shop = $shop->getData();
+            foreach($shop['id_category'] as $id){
+                if(array_key_exists($id,$filter)){
+                    $sub = [
+                        'name' => (isset($shop['name'])) ? $shop['name'] : '',
+                        'src' => (isset($filter[$id]['src'])) ? Mage::getBaseUrl('media').'catalog/category/'.$filter[$id]['src'] : Mage::getBaseUrl('media').'resource/commerçant_dummy.png',
+                        'adresse' => (isset($shop['street'])) ? $shop['street'].' '.$shop['postcode'].' '.$shop['city'] : '',
+                        'url' => (isset($filter[$id]['url_path'])) ? Mage::getUrl($filter[$id]['url_path']) : '',
+                    ];
+                    $data[] = $sub;
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    public function getInfoShop()
+    {
+        $shop_info=array();
+        $current_cat=Mage::registry('current_category');
+        $data = Mage::getSingleton('apdc_commercant/shop')->getCollection()->addFieldToFilter('id_category', array('finset' =>$current_cat->getId()))->getFirstItem()->getData();
+
+        $shop_info["name"]=$data["name"];
+        $shop_info["adresse"]=$data["street"]." ".$data["postcode"]." ".$data["city"];
+        $shop_info["url_adresse"]="https://www.google.fr/maps/place/".str_replace(" ","+", $shop_info["adresse"]);
+        $shop_info["phone"]=$data["phone"];
+        $shop_info["website"]=$data["website"];
+        $shop_info["closing_periods"]=$data["closing_periods"];;
+        $shop_info["description"]=$current_cat->getDescription();
+        $shop_info["delivery_days"]=Mage::helper('apdc_commercant')->formatDays($data["delivery_days"],true);
+        $shop_info["image"]=$current_cat->getImageURL();
+
+        $html="";
+        $days=["Lun","Mar","Mer","Jeu","Ven","Sam","Dim"];
+        foreach($data["timetable"] as $day=>$hours){
+            $hours=($hours=="")?"Fermé":$hours;
+            $html.=$days[$day].": ".$hours."</br>";
+        }
+        $shop_info["timetable"]=$html;
+
+        return $shop_info;
+    }
+}
---
+++ skin/frontend/boilerplate/default/js/apdc_popup/newsletter-popup.js
@@ -0,0 +1,77 @@
+Validation.add('required-entry', "Merci de compléter ce champ!", function(v) {
+        return !Validation.get('IsEmpty').test(v);
+});
+
+jQuery(document).ready(function() {
+
+    if (typeof(apdcNewsletterPopup) === 'undefined') {
+      apdcNewsletterPopup = new ApdcPopup({
+          id: 'newsletter-form'
+      });
+    }
+
+    jQuery(document).on('click', '#newsletter-popup',function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        apdcNewsletterPopup.showLoading();
+        showNewsletterForm(this,'apdc_newsletter_view');
+    });
+
+    jQuery(document).on('submit','#newsletter-form', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        processNewsletterForm(this);
+    });
+
+    function showNewsletterForm(elt,handle) {
+        var ajaxUrl = jQuery(elt).data('newsletter-view');
+        var data = new FormData();
+        data.append('isAjax', 1);
+        data.append('handle', handle);
+        jQuery.ajax({
+            url: ajaxUrl,
+            data: data,
+            processData: false,
+            contentType: false,
+            type: 'POST'
+
+        })
+        .done(function(response) {
+            if (response.status === 'SUCCESS') {
+                apdcNewsletterPopup.updateContent(response.html);
+            } else if (response.status === 'ERROR') {
+                var message = '<ul class="messages"><li class="notice-msg"><ul><li><span>' + response.message + '</span></li></ul></li></ul>';
+                apdcNewsletterPopup.updateContent(message);
+            }
+        })
+        .fail(function() {
+            console.log('failed');
+        });
+
+    }
+
+    function processNewsletterForm(elt) {
+        var ajaxUrl = jQuery(elt).attr('action');
+        var data = new FormData(jQuery(elt)[0]);
+        data.append("isAjax", 1);
+        jQuery(elt).children(".form-group").children("input").attr("disabled", true);
+        jQuery(elt).children(".form-group").children("select").attr("disabled", true);
+        jQuery(elt).children("button").attr("disabled", true).removeClass("button-green");
+        jQuery.ajax({
+                url: ajaxUrl,
+                data: data,
+                processData: false,
+                contentType: false,
+                type: 'POST'
+
+            })
+            .done(function(response) {
+                response = JSON.parse(response);
+                apdcNewsletterPopup.updateContent(response.html);
+            })
+            .fail(function() {
+                console.log('failed');
+            });
+    }
+
+});
