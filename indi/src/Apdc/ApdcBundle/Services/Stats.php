3845ebc2d65af83c1e89ebf6650e87adafef2b42
Pierre Mainguet <mainguetpierre@gmail.com>
2017-04-12 13:42:23 +0200

    small fix in Stats service in Indi

--- indi/src/Apdc/ApdcBundle/Services/Stats.php
+++ indi/src/Apdc/ApdcBundle/Services/Stats.php
@@ -250,13 +250,12 @@ class Stats
 		$date_debut = date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $date_debut)));
 		$notationClient = \Mage::getModel('sales/order')->getCollection()
 			->addAttributeToFilter('created_at', array('from' => $date_debut, 'to' => $date_fin));
-		$notationClient->getSelect()->joinLeft('apdc_notation', 'main_table.increment_id = apdc_notation.order_id');
+		$notationClient->getSelect()->joinLeft('apdc_notation', 'main_table.entity_id = apdc_notation.order_id');
 		$notationClient->getSelect()->joinLeft('mwddate_store', 'main_table.entity_id = mwddate_store.sales_order_id', array('mwddate_store.ddate_id'));
 		$notationClient->getSelect()->joinLeft('mwddate', 'mwddate_store.ddate_id = mwddate.ddate_id', array('ddate' => 'mwddate.ddate'));
 
-
-
 		$result = [];
+
 		foreach ($notationClient as $n) {
 			$result[] = [
 				'date_creation'		=> date('d/m/Y', strtotime($n->getCreatedAt())),
