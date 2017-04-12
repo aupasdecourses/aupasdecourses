4f25d7be6498ef0b195ceaf478298a046a15d7a2
sturquier <turquiersimon@hotmail.fr>
2017-04-12 16:16:52 +0200

    map merchants + customers. First implement

---
+++ indi/src/Apdc/ApdcBundle/Controller/MapController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Apdc\ApdcBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MapController extends Controller 
+{
+	public function merchantsAction()
+	{
+		return $this->render('ApdcApdcBundle::map/merchants.html.twig');
+	}
+
+	public function customersAction()
+	{
+		return $this->render('ApdcApdcBundle::map/customers.html.twig');
+	}
+}	
