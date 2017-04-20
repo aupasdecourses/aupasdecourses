1cf93fa2386e9390d3904e993a774dae39858f26
sturquier <turquiersimon@hotmail.fr>
2017-04-20 15:08:14 +0200

    add former adress for join between geocode and sales_order

---
+++ app/code/local/Apdc/Delivery/sql/pmainguet_delivery_setup/mysql4-upgrade-0.0.9-0.1.0.php
@@ -0,0 +1,20 @@
+<?php
+
+/** Ajout de la colonne former_address
+ *	dans la table geocode_customers
+ *	ainsi deux colonnes adresses : une pour la jointure (former)
+ *	et une colonne adresse propre pour encodage latitude longitude
+ */
+
+$installer = $this;
+$installer->startSetup();
+
+$table = $installer->getConnection()
+	->addColumn($installer->getTable('pmainguet_delivery/geocode_customers'),'former_address', array(
+		'type'		=>	Varien_Db_Ddl_Table::TYPE_TEXT,
+		'nullable'	=> true,
+		'default'	=> null,
+		'comment'	=> 'Ancienne adresse pour jointure sales_order',
+	));
+
+$installer->endSetup();
