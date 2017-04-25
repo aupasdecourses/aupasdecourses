0de11518abf997710ef18f1169b8aeb54f3462e8
Nicolas Boullay <n.boullay@touchfordiffusion.com>
2017-04-25 15:46:19 +0200

    Style checkout

--- app/code/local/Apdc/Checkout/Block/Onepage/Checkcart.php
+++ app/code/local/Apdc/Checkout/Block/Onepage/Checkcart.php
@@ -5,7 +5,7 @@ class Apdc_Checkout_Block_Onepage_Checkcart extends Mage_Checkout_Block_Onepage_
     protected function _construct()
     {
         $this->getCheckout()->setStepData("checkcart", array(
-            "label"     => Mage::helper("checkout")->__("Check cart"),
+            "label"     => Mage::helper("checkout")->__("<span>3/4</span> Vos produits"),
             "is_show"   => $this->isShow()
         ));
         parent::_construct();
