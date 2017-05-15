2830524c9623e7ba5dd9bed20a2e16979bb37d39
sturquier <turquiersimon@hotmail.fr>
2017-05-15 15:47:09 +0200

    up with uglify & beautify

--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,67 @@ var gulp		= require('gulp');
 var plugins		= require('gulp-load-plugins')();
 var indi		= './indi';
 
-/* Reordonner declarations CSS + reindentation et reformatage */
-gulp.task('css', function () {
+/** CSS **/
+
+/* Reordonner declarations, reindentation et reformatage */
+gulp.task('cleanCss', function () {
 	return gulp.src(indi + '/web/css/*.css')
 			.pipe(plugins.csscomb())
-			.pipe(plugins.cssbeautify({indent: '  '}))
+			.pipe(plugins.cssbeautify({indent_size: 2}))
 			.pipe(gulp.dest(indi + '/web/css/'));
 	});
 
-gulp.task('cssMap', function () {
+gulp.task('cleanCssMap', function () {
 	return gulp.src(indi + '/web/css/map/*.css')
 			.pipe(plugins.csscomb())
-			.pipe(plugins.cssbeautify({indent: '  '}))
+			.pipe(plugins.cssbeautify({indent_size: 2}))
 			.pipe(gulp.dest(indi + '/web/css/map/'));
 	});
 
-
-/* Minification CSS */
-gulp.task('minify', function () {
+/* Minifier */
+gulp.task('minifyCss', function () {
 	return gulp.src(indi + '/web/css/*.css')
 			.pipe(plugins.csso())
 			.pipe(gulp.dest(indi + '/web/css/'));
 	});
 
-gulp.task('minifyMap', function () {
+gulp.task('minifyCssMap', function () {
 	return gulp.src(indi + '/web/css/map/*.css')
 			.pipe(plugins.csso())
 			.pipe(gulp.dest(indi + '/web/css/map/'));
 	});
 
 
-// Run gulp indi at www/
-gulp.task('indi', ['css', 'cssMap', 'minify', 'minifyMap']);
+/** JS **/
+
+/* Reordonner decla, reindentation et reformatage */
+gulp.task('cleanJS', function () {
+	return gulp.src(indi + '/web/js/*.js')
+			.pipe(plugins.beautify({indent_size: 2}))
+			.pipe(gulp.dest(indi + '/web/js/'));
+	});
+
+gulp.task('cleanJSMap', function () {
+	return gulp.src(indi + '/web/js/map/*.js')
+			.pipe(plugins.beautify({indent_size: 2}))
+			.pipe(gulp.dest(indi + '/web/js/map/'));
+	});
+
+
+/* Minifier */
+gulp.task('minifyJS', function () {
+	return gulp.src(indi + '/web/js/*.js')
+			.pipe(plugins.uglify())
+			.pipe(gulp.dest(indi + '/web/js/'));
+	});
+
+gulp.task('minifyJSMap', function () {
+	return gulp.src(indi + '/web/js/map/*.js')
+			.pipe(plugins.uglify())
+			.pipe(gulp.dest(indi + '/web/js/map/'));
+	});
+
+
+
+// Run 'gulp indi' at www/
+gulp.task('indi', ['cleanCss', 'cleanCssMap', 'minifyCss', 'minifyCssMap', 'cleanJS', 'cleanJSMap', 'minifyJS', 'minifyJSMap']);
