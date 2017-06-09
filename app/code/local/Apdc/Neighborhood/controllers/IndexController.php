8e0777caafc59d6529c55669c494d68911cf47f1
Erwan INYZANT <erwan.inyzant@otogo.com>
2017-06-09 14:05:38 +0700

    NEW : add admin notification when customer change Neighborhood + add some configuration.

--- app/code/local/Apdc/Neighborhood/controllers/IndexController.php
+++ app/code/local/Apdc/Neighborhood/controllers/IndexController.php
@@ -37,8 +37,17 @@ class Apdc_Neighborhood_IndexController extends Mage_Core_Controller_Front_Actio
                 $neighborhood = Mage::getModel('apdc_neighborhood/neighborhood')->load((int)$id);
                 $customer = $this->_getSession()->getCustomer();
                 if ($customer && $customer->getId() && $neighborhood && $neighborhood->getId()) {
+                    $oldNeighborhoodId = $customer->getCustomerNeighborhood();
                     $customer->setCustomerNeighborhood($neighborhood->getId())
                         ->save();
+
+                    if (Mage::getStoreConfig('apdc_neighborhood/notifications/is_active')) {
+                        $oldNeighborhood = Mage::getModel('apdc_neighborhood/neighborhood');
+                        if ($oldNeighborhoodId) {
+                            $oldNeighborhood = $oldNeighborhood->load($oldNeighborhoodId);
+                        }
+                        Mage::helper('apdc_neighborhood')->sendChangeNeighborhoodAdminNotification($customer, $oldNeighborhood, $neighborhood);
+                    }
                     Mage::getSingleton('core/session')->addSuccess(Mage::helper('apdc_neighborhood')->__('Your neighborhood is now %s', $neighborhood->getName()));
                     return $this->_redirectUrl($neighborhood->getStoreUrl());
                 }
