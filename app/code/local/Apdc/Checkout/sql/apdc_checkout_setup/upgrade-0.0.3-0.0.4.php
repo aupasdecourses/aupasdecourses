1b7ad0566f28611aab0f84c0468a361086272bca
Pierre Mainguet <mainguetpierre@gmail.com>
2017-09-02 16:19:47 +0200

    fix pb with produit_equivalent and comments in checkout, backend, indi + add 1e, 2e, 8e script for boutique

---
+++ app/code/local/Apdc/Checkout/sql/apdc_checkout_setup/upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var Mage_Core_Model_Resource_Setup $installer */
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
+/**
+ * Add 'custom_attribute' attribute for entities
+ */
+$entities = array(
+    'order',
+);
+$options = array(
+    'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
+    'visible'  => true,
+    'required' => false,
+	'default'  => 1
+);
+foreach ($entities as $entity) {
+    $installer->addAttribute($entity, 'produit_equivalent', $options);
+}
+$installer->endSetup();
