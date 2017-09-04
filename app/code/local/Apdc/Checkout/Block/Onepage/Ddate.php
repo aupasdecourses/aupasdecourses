42b1852a00c94ac00b5e6b94c162fd91a9e3221c
Erwan INYZANT <erwan.inyzant@otogo.com>
2017-09-04 11:50:57 +0700

    NEW : design for choosing date time for Delivery -> First select day and then select hours

---
+++ app/code/local/Apdc/Checkout/Block/Onepage/Ddate.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of the GardenMedia Mission Project 
+ * 
+ * @category Apdc
+ * @package  Checkout
+ * @author   Erwan INYZANT <erwan@garden-media.fr> 
+ * @license  All right reserved to Garden Media Studio VN Company Limited
+ * @link     http://www.garden-media.fr
+ */
+/**
+ * Apdc_Checkout_Block_Onepage_Ddate 
+ * 
+ * @category Apdc
+ * @package  Checkout
+ * @uses     MW
+ * @uses     MW_Ddate_Block_Onepage_Ddate
+ * @author   Erwan INYZANT <erwan@garden-media.fr> 
+ * @license  All right reserved to Garden Media Studio VN Company Limited
+ * @link     http://www.garden-media.fr
+ */
+class Apdc_Checkout_Block_Onepage_Ddate extends MW_Ddate_Block_Onepage_Ddate
+{
+    /**
+     * getAvailableDaysAndSlots 
+     * 
+     * @return array
+     */
+    public function getAvailableDaysAndSlots($withUnaivalableDays=true)
+    {
+        $availableDaysAndSlots = [];
+        $slots = $this->getSlots();
+        $currentTime = Mage::getSingleton('core/date')->timestamp();
+        $cpt = 0;
+        $day = 0;
+        if ($withUnaivalableDays) {
+            for ($day = 0; $day < 7; $day++) {
+                $strDate = date('Y-m-d', strtotime('+' . $day . ' day', $currentTime));
+                $date_FR = Mage::app()->getLocale()->date(strtotime('+' . $day . ' day', $currentTime));
+                $hasSlot = false;
+                if (!isset($availableDaysAndSlots[$strDate])) {
+                    $availableDaysAndSlots[$strDate] = [
+                        'day' => [
+                            'day' => $date_FR->get(Zend_Date::WEEKDAY_SHORT),
+                            'date' => date(Mage::helper('ddate')->month_date_format(), strtotime('+' . $day . ' day', $currentTime))
+                        ],
+                        'slots' => [
+                        ]
+                    ];
+                }
+                foreach ($slots as $slot) {
+                    if($this->isEnabled($slot->getId(), $strDate)){
+                        $hasSlot = true;
+                        $availableDaysAndSlots[$strDate]['slots'][$slot->getDtimeId()] = [
+                            'dtime_id' => $slot->getDtimeId(),
+                            'dtime' => $slot->getDtime()
+                        ];
+                    }
+                }
+                $availableDaysAndSlots[$strDate]['has_slot'] = $hasSlot;
+            }
+        } else {
+            while($cpt < 7) {
+                $strDate = date('Y-m-d', strtotime('+' . $day . ' day', $currentTime));
+                $date_FR = Mage::app()->getLocale()->date(strtotime('+' . $day . ' day', $currentTime));
+                $hasSlot = false;
+                foreach ($slots as $slot) {
+                    if($this->isEnabled($slot->getId(), $strDate)){
+                        $hasSlot = true;
+                        if (!isset($availableDaysAndSlots[$strDate])) {
+                            $availableDaysAndSlots[$strDate] = [
+                                'day' => [
+                                    'day' => $date_FR->get(Zend_Date::WEEKDAY_SHORT),
+                                    'date' => date(Mage::helper('ddate')->month_date_format(), strtotime('+' . $day . ' day', $currentTime))
+                                ],
+                                'slots' => [
+                                ]
+                            ];
+                        }
+                        $availableDaysAndSlots[$strDate]['slots'][$slot->getDtimeId()] = [
+                            'dtime_id' => $slot->getDtimeId(),
+                            'dtime' => $slot->getDtime()
+                        ];
+                    }
+                }
+                if ($hasSlot) {
+                    $cpt++;
+                }
+                $day++;
+            }
+        }
+        return $availableDaysAndSlots;
+    }
+}
---
+++ skin/frontend/boilerplate/default/js/apdc_checkout/ddate_timepicker.js
@@ -0,0 +1,101 @@
+var Apdc_DdateTimePicker = function(options) {
+  var defaults = {
+    'saveUrl': '',
+    'redirectUrl':'',
+    'daysAndSlots' : {},
+    'container': '#co-ddate-form .delivery'
+  };
+  var apdc_ddateTimePicker = {
+    options: {},
+    currentDate: null,
+    currentTime: null,
+    init: function(options) {
+      this.options = jQuery.extend(defaults, options);
+      this.isInPopup = (jQuery(this.options.container).parents('.apdc-popup').length > 0);
+      this.initPreselected();
+      this.initActions();
+    },
+
+    initActions: function() {
+      var self = this;
+      jQuery(this.options.container).find('.select-days li.available').on('click', function() {
+        self.currentDate = jQuery(this).data('date');
+        self.currentTime = null;
+        self.refreshCurrentDate();
+      });
+      jQuery(document).on('click', this.options.container + ' .select-time li', function() {
+        self.currentTime = jQuery(this).data('time');
+        self.refreshCurrentTime();
+      });
+    },
+    initPreselected: function() {
+      if (typeof (this.options.currentDate) !== 'undefined' &&
+        this.options.currentDate !== ''
+      ) {
+        this.currentDate = this.options.currentDate;
+        if (typeof(this.options.currentTime) !== 'undefined' &&
+          this.options.currentTime !== ''
+        ) {
+          this.currentTime = this.options.currentTime;
+        }
+        this.refreshCurrentDate();
+      }
+    },
+    refreshCurrentDate: function() {
+      jQuery(this.options.container + ' .select-days li.selected').removeClass('selected');
+      if (this.currentDate && this.currentDate !== '') {
+        if (this.isInPopup && typeof(apdcDeliveryPopup) !== 'undefined') {
+          apdcDeliveryPopup.showLoading();
+          window.setTimeout(function() {
+            apdcDeliveryPopup.hideLoading();
+          }, 200);
+        }
+        jQuery(this.options.container + ' .select-days li[data-date="' + this.currentDate + '"]').addClass('selected');
+        var slots = this.options.daysAndSlots[this.currentDate].slots;
+        var chooseTime = '';
+        for (var slot in slots) {
+          var selected = (this.currentDate === this.options.currentDate && slots[slot].dtime === this.options.currentTime ? ' selected' : '');
+          chooseTime += '<li class="available' + selected + '" data-time="' + slots[slot].dtime_id + '"><div>' + slots[slot].dtime + '</div></li>';
+        }
+        chooseTime += '<li class="availabl" data-time=""><div>15h00-18h00</div></li>';
+        jQuery(this.options.container + ' .select-time ul').html(chooseTime);
+        jQuery(this.options.container + ' .select-time').removeClass('hide');
+        if (this.isInPopup && typeof(apdcDeliveryPopup) !== 'undefined') {
+          apdcDeliveryPopup.initPopupHeight();
+        }
+      }
+    },
+    refreshCurrentTime: function() {
+      jQuery(this.options.container + ' .select-time li.selected').removeClass('selected');
+      if (this.currentTime && this.currentTime !== '') {
+        jQuery(this.options.container + ' .select-time li[data-time="' + this.currentTime + '"]').addClass('selected');
+        this.saveSelectedDateTime();
+      }
+    },
+    saveSelectedDateTime: function() {
+      var self = this;
+
+      if (this.isInPopup && typeof(apdcDeliveryPopup) !== 'undefined') {
+        apdcDeliveryPopup.showLoading();
+      }
+      jQuery.ajax( {
+        url : self.options.saveUrl,
+        type: 'post',
+        dataType : 'json',
+        data:{
+          'date':self.currentDate,
+          'dtime':self.currentTime,
+          'ddatei':'',
+          'url':self.options.redirectUrl
+        },
+        success : function(data) {
+          if (self.isInPopup) {
+            window.location.reload();
+          }
+        }
+      });
+    }
+  };
+  apdc_ddateTimePicker.init(options);
+  return apdc_ddateTimePicker;
+};
