9447346fabfdea71c4c0fa9d110e1a50f49e26bb
sturquier <turquiersimon@hotmail.fr>
2017-09-12 17:56:12 +0200

    Forgot to rename form

--- indi/src/Apdc/ApdcBundle/Form/IndiPayoutType.php
+++ indi/src/Apdc/ApdcBundle/Form/IndiPayoutType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class PayoutType extends AbstractType
+class IndiPayoutType extends AbstractType
 {
 
 	public function buildForm(FormBuilderInterface $builder, array $options)
