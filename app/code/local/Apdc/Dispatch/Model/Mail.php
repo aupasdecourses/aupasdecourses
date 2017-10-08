0a01dca73ac287a8fd8fa0375c6cfae99d044846
Pierre Mainguet <mainguetpierre@gmail.com>
2017-10-08 12:51:17 +0200

    fix group emails by shop and not by store/shop for mails + saturdays cut-off for mistral

--- app/code/local/Apdc/Dispatch/Model/Mail.php
+++ app/code/local/Apdc/Dispatch/Model/Mail.php
@@ -107,7 +107,7 @@ class Apdc_Dispatch_Model_Mail extends Mage_Core_Model_Abstract
                             $mail = new Mandrill_Message(Mage::getStoreConfig(Ebizmarts_Mandrill_Model_System_Config::APIKEY));
 
                             $mail->addTo($emails);
-                            //$mail->addBcc(Mage::getStoreConfig('trans_email/ident_general/email'));
+                            $mail->addBcc(Mage::getStoreConfig('trans_email/ident_general/email'));
                             $mail->setFrom(Mage::getStoreConfig('trans_email/ident_general/email'), "L'équipe d'Au Pas De Courses");
                             $mail->setSubject("Au Pas De Courses {$pdf->getOrdersCount()} commandes le {$this->_c_date}");
                             $mail->setBodyHtml(
