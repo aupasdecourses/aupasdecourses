8eb138152dd342d7d3556c30626d6f6a44af877c
Pierre Mainguet <mainguetpierre@gmail.com>
2017-10-11 17:11:26 +0200

    fix deliverycheck

--- app/code/local/Apdc/Attributesqtoi/Model/Observer.php
+++ app/code/local/Apdc/Attributesqtoi/Model/Observer.php
@@ -15,29 +15,4 @@ class Apdc_Attributesqtoi_Model_Observer extends Varien_Object
 		$quoteItem->setProduitFragile($product->getProduitFragile());
     }
 
-    public function deliverycheck($observer){
-        try{
-            $orderItem = $observer->getOrderItem();
-            $quoteItem = $observer->getEvent()->getItem();
-            $ddate=Mage::getSingleton('core/session')->getDdate();
-
-            if(isset($ddate)){
-                $timestamp = strtotime($ddate);
-                $day = (date('w', $timestamp)==0)?7:date('w', $timestamp);
-                $delivery_days = Mage::helper('apdc_catalog/product_available')->getDeliveryDays($quoteItem->getProduct());
-                if(!in_array($day,$delivery_days)){
-                    if(count($delivery_days)==0){
-                        $orderItem->setDeliveryCheck('Non disponible cette semaine');
-                    }elseif(count($delivery_days)<4){
-                        $orderItem->setDeliveryCheck('Non disponible pour ce jour de livraison');
-                    }
-                }else{
-                    $orderItem->setDeliveryCheck('OK');
-                }
-            }
-        }catch(Exception $e){
-            Mage::log('Error deliverycheck observer, see below',null,'deliverycheck.log');
-            Mage::log($e->getMessage(),null,'deliverycheck.log');
-        }
-    }
 }
