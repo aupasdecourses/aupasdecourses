bc395abd43bfbe50a09414ae425ff14dea5617a1
sturquier <turquiersimon@hotmail.fr>
2018-03-22 12:09:33 +0100

    Remove screenshot comment

--- app/code/local/Apdc/Delivery/sql/pmainguet_delivery_setup/mysql4-upgrade-0.1.8-0.1.9.php
+++ app/code/local/Apdc/Delivery/sql/pmainguet_delivery_setup/mysql4-upgrade-0.1.8-0.1.9.php
@@ -3,5 +3,6 @@
 $this->startSetup();
 
 $this->getConnection()->dropColumn($this->getTable('amorderattach/order_field'), 'commentaires_commande');
+$this->getConnection()->dropColumn($this->getTable('amorderattach/order_field'), 'screenshot');
 
 $this->endSetup();
