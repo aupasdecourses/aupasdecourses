56f4102d729ba314eb3f4c57252519873a5aafd6
sturquier <turquiersimon@hotmail.fr>
2018-03-22 14:43:01 +0100

    Add new table indi_commenthistory

---
+++ app/code/local/Apdc/Delivery/Model/Indi/Commenthistory.php
@@ -0,0 +1,9 @@
+<?php
+
+class Apdc_Delivery_Model_Indi_Commenthistory extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('pmainguet_delivery/indi_commenthistory');
+    }
+}
---
+++ app/code/local/Apdc/Delivery/Model/Resource/Indi/Commenthistory.php
@@ -0,0 +1,8 @@
+<?php
+
+class Apdc_Delivery_Model_Resource_Indi_Commenthistory extends Mage_Core_Model_Resource_Db_Abstract{
+    protected function _construct()
+    {
+        $this->_init('pmainguet_delivery/indi_commenthistory', 'id');
+    }
+}
