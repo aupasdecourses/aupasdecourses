d81d7f58912ec21eb5f612e5eb8fecbff53c5f12
sturquier <turquiersimon@hotmail.fr>
2018-07-09 11:20:20 +0200

    Better implementation of catalog/product mock

--- indi/tests/Apdc/ApdcBundle/Services/Helpers/ModelTest.php
+++ indi/tests/Apdc/ApdcBundle/Services/Helpers/ModelTest.php
@@ -18,9 +18,28 @@ class ModelTest extends TestCase
 
 		$this->catalogProductClass = get_class(\Mage::getModel('catalog/product'));
 		$this->catalogProductData = [
-			'getEntityId' 	=> 99999,
-			'getOptions'	=> 0,
-			'getSku'		=> 'AAA01-ABC-1234',
+			[
+				'entity_id' 		=> 12345,
+				'entity_type_id'	=> 4,
+				'attribute_set_id'	=> 4,
+				'type_id'			=> 'simple',
+				'sku'				=> 'AAA01-ABC-1234',
+				'has_options'		=> 0,
+				'required_options'	=> 0,
+				'created_at'		=> '2018-01-01 00:00:00',
+				'updated_at'		=> '2018-01-01 12:45:57',
+			],
+			[
+				'entity_id' 		=> 56789,
+				'entity_type_id'	=> 4,
+				'attribute_set_id'	=> 4,
+				'type_id'			=> 'simple',
+				'sku'				=> 'BBB05-DEF-5678',
+				'has_options'		=> 1,
+				'required_options'	=> 0,
+				'created_at'		=> '2018-02-10 15:55:23',
+				'updated_at'		=> '2018-02-10 16:12:45',
+			]
 		];
 	}
 
@@ -37,14 +56,20 @@ class ModelTest extends TestCase
 	 */
 	public function testCatalogProductMock()
 	{
-		$product = $this->createMock($this->catalogProductClass);
+		$products = $this->createMock($this->catalogProductClass);
 		
-		foreach ($this->catalogProductData as $key => $value) {
-			$product->method($key)->willReturn($value);
-		}
+		$products->method('getCollection')->willReturn($this->catalogProductData);
 
-		$this->assertInternalType("int", $product->getEntityId());
-		$this->assertTrue($product->getOptions() == false);
-		$this->assertEquals('AAA01-ABC-1234', $product->getSku());
+		foreach ($products->getCollection() as $product) {
+			$this->assertInternalType('int', $product['entity_id']);
+			$this->assertInternalType('int', $product['entity_type_id']);
+			$this->assertInternalType('int', $product['attribute_set_id']);
+			$this->assertEquals('simple', $product['type_id']);
+			$this->assertRegExp("/^[A-Za-z0-9]{5}-[A-Za-z]{3}-[0-9]{4}/", $product['sku']);
+			$this->assertInternalType('int', $product['has_options']);
+			$this->assertInternalType('int', $product['required_options']);
+			$this->assertRegExp("/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/", $product['created_at']);
+			$this->assertRegExp("/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/", $product['updated_at']);
+		}
 	}
 }
