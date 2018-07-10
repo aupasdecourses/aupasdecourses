dc0371be8bee62f4886adfc25e59a3994ae8ab2b
sturquier <turquiersimon@hotmail.fr>
2018-07-10 14:41:26 +0200

    Mock refund_items table

--- indi/tests/Magento/Models/AbstractModelTest.php
+++ indi/tests/Magento/Models/AbstractModelTest.php
@@ -9,6 +9,7 @@ class AbstractModelTest extends TestCase
 {
 	protected $catalog_product_class;
 	protected $amasty_order_field_class;
+	protected $refund_items_class;
 
 	public function setUp()
 	{
@@ -16,11 +17,13 @@ class AbstractModelTest extends TestCase
 
 		$this->catalog_product_class 			= get_class(\Mage::getModel('catalog/product'));
 		$this->amasty_order_field_class 		= get_class(\Mage::getModel('amorderattach/order_field'));
+		$this->refund_items_class 				= get_class(\Mage::getModel('pmainguet_delivery/refund_items'));
 	}
 
 	public function tearDown()
 	{
 		$this->catalog_product_class 		= null;
 		$this->amasty_order_field_class		= null;
+		$this->refund_items_class 			= null;
 	}
 }
